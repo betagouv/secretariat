2ddfcb08a90342fe63319aa07b4e22a6c924d2d3
Alejandro MG <alejandro@amguillen.dev>
2020-07-31 12:35:08 +0200

    First draft of the automatic generation of onboarders (#80)

---
+++ controllers/onboardingController.js
@@ -0,0 +1,146 @@
+const config = require('../config');
+const BetaGouv = require('../betagouv');
+const buildBetaEmail = require('./utils').buildBetaEmail;
+const sendMail = require('./utils').sendMail;
+const jwt = require('jsonwebtoken');
+
+async function selectRandomOnboarder(newcomerId) {
+  const users = await BetaGouv.usersInfos();
+  const minimumSeniority = new Date().setMonth(new Date().getMonth() - 6);
+  const onboarders = users.filter(x => {
+    const senior = new Date(minimumSeniority) > new Date(x.start);
+    const stillActive = !x.end || new Date(x.end) > new Date();
+    const isRequester = x.id === newcomerId;
+    return senior && stillActive && !isRequester;
+  });
+  const onboarder = onboarders[Math.floor(Math.random() * onboarders.length)];
+  return onboarder;
+}
+
+function getDataFromToken(token) {
+  if (!token) {
+    throw new Error('Token missing');
+  }
+  const jwtIsValid = jwt.verify(token, config.secret);
+  if (!jwtIsValid) {
+    throw new Error('Invalid token in link');
+  }
+  const data = jwt.decode(token, { json: true });
+  if (!data) {
+    throw new Error('Corrupted data in token');
+  }
+  return data;
+}
+
+async function sendOnboarderRequestEmail(onboarder, newcomer, req) {
+  const url = `${config.secure ? 'https' : 'http'}://${req.hostname}`;
+
+  const token = jwt.sign({
+    newcomer: newcomer,
+    onboarder: onboarder
+  }, config.secret);
+
+  const html = `
+    <h1>Hello ${onboarder.fullname} 👋,</h1>
+    <p>Tu as été sélectonné.e aléatoirement pour devenir <a href="https://doc.incubateur.net/communaute/travailler-a-beta-gouv/bienvenue/marrainage">marrain.ne</a> de ${newcomer.fullname}.</p>
+    <a href="${url}/onboardingRequest/accept?details=${encodeURIComponent(token)}">
+      <button style="margin-bottom: 15px;background: green;padding: 10px;border: none;border-radius: 3px;color: white;min-width: 280px;box-shadow: 1px 1px 2px 0px #333;cursor: pointer;">
+        J'accepte
+      </button>
+    </a><br/>
+    <a href="${url}/onboardingRequest/decline?details=${encodeURIComponent(token)}">
+      <button style="padding: 10px;border: none;box-shadow: 1px 1px 2px 0px #333;min-width: 280px;">
+        Désolé, je ne suis pas disponible
+      </button>
+    </a>
+    <p>Bonne journée,</p>
+    <p>🤖 Le sécretariat</p>
+  `;
+
+  try {
+    return await sendMail(buildBetaEmail(onboarder.id), `Tu as été sélectionné.e comme marrain.ne 🙌`, html);
+  } catch (err) {
+    throw new Error(`Erreur d'envoi de mail à l'adresse indiqué ${err}`);
+  }
+}
+
+module.exports.createOnboardingRequest = async function (req, res) {
+  try {
+    const newcomer = await BetaGouv.userInfosById(req.body.newcomerId);
+    const onboarder = await selectRandomOnboarder(newcomer.id);
+    await sendOnboarderRequestEmail(onboarder, newcomer, req)
+    await BetaGouv.sendInfoToSlack(`${newcomer.fullname} a demandé un.e marrain.ne.`);
+
+    req.flash('message', `Nous avons envoyé un email à ${onboarder.fullname} l'invitant à te marrainer.`);
+    res.redirect(`/users/${newcomer.id}`);
+  } catch (err) {
+    console.error(err);
+
+    req.flash('error', err.message);
+    res.redirect(`/users/${id}`);
+  }
+}
+
+module.exports.acceptOnboardingRequest = async function (req, res) {
+  try {
+    const details = getDataFromToken(req.query.details);
+    const newcomer = details.newcomer;
+    const onboarder = details.onboarder;
+
+    const html = `
+      <h1>Hello ${newcomer.fullname}, ${onboarder.fullname} 👋,</h1>
+      <p>${onboarder.fullname} a accepté d'être marrain.ne de ${newcomer.fullname}.</p>
+      <p>Vous trouverez plus d'informations sur le marrainage sur la <a href="https://doc.incubateur.net/communaute/travailler-a-beta-gouv/bienvenue/marrainage">documentation de l'incubateur</a>.</p>
+      <p>Vous êtes tou.s.tes les deux en copie de cet email, à vous de jouer ! </p>
+      <p>Bonne journée,</p>
+      <p>🤖 Le sécretariat</p>
+    `;
+
+    try {
+      await sendMail([buildBetaEmail(onboarder.id), buildBetaEmail(newcomer.id)], `Mise en contact pour marrainage`, html);
+    } catch (err) {
+      throw new Error(`Erreur d'envoi de mail à l'adresse indiqué ${err}`);
+    }
+
+    req.flash('message', `Vous êtes le/la marrain.ne de ${newcomer.fullname}, vous recevrez un email de confirmation.`);
+    res.redirect(`/users/${newcomer.id}`);
+  } catch (err) {
+    console.error(err);
+
+    req.flash('error', err.message);
+    res.redirect(`/users`);
+  }
+}
+
+module.exports.declineOnboardingRequest = async function (req, res) {
+  try {
+    const details = getDataFromToken(req.query.details);
+    const newcomer = details.newcomer;
+    const declinedOnboarder = details.onboarder;
+
+    const onboarder = await selectRandomOnboarder(newcomer.id);
+    await sendOnboarderRequestEmail(onboarder, newcomer, req);
+
+    const html = `
+      <h1>Hello 👋 ${newcomer.fullname},</h1>
+      <p>Malheureusement, ${declinedOnboarder.fullname} n'est pas disponible en ce moment.</p>
+      <p>Nous avons envoyé une demande de marrainage à ${onboarder.fullname}.</p>
+      <p>Bonne journée,</p>
+      <p>🤖 Le sécretariat</p>
+    `;
+
+    try {
+      await sendMail([buildBetaEmail(newcomer.id)], `La recherche de marrain.ne se poursuit !`, html);
+    } catch (err) {
+      throw new Error(`Erreur d'envoi de mail à l'adresse indiqué ${err}`);
+    }
+
+    req.flash('message', `Merci de votre réponse, elle a bien été prise en compte.`);
+    res.redirect(`/users/${newcomer.id}`);
+  } catch (err) {
+    console.error(err);
+
+    req.flash('error', err.message);
+    res.redirect(`/users`);
+  }
+}
--- index.js
+++ index.js
@@ -15,6 +15,7 @@ logoutController = require('./controllers/logoutController');
 loginController = require('./controllers/loginController');
 emailsController = require('./controllers/emailsController');
 usersController = require('./controllers/usersController');
+onboardingController = require('./controllers/onboardingController');
 
 const app = express();
 
@@ -78,5 +79,8 @@ app.post('/users/:id/email', usersController.createEmailForUser);
 app.post('/users/:id/redirections', usersController.createRedirectionForUser);
 app.post('/users/:id/redirections/:email/delete', usersController.deleteRedirectionForUser);
 app.post('/users/:id/password', usersController.updatePasswordForUser);
+app.post('/onboardingRequest', onboardingController.createOnboardingRequest);
+app.get('/onboardingRequest/accept', onboardingController.acceptOnboardingRequest);
+app.get('/onboardingRequest/decline', onboardingController.declineOnboardingRequest);
 
 module.exports = app.listen(config.port, () => console.log(`Running on port: ${config.port}`));
