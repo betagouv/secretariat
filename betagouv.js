05e39b0e0dceeb94c151f4b1b5db63d248fad3f2
Raphael Odini <raphael.odini@protonmail.com>
2020-07-31 12:16:11 +0200

    forgot to save file

--- betagouv.js
+++ betagouv.js
@@ -17,8 +17,8 @@ const config = {
 };
 
 const betaOVH = {
-  emailInfos: async name => {
-    const url = `/email/domain/${config.domain}/account/${name}`;
+  emailInfos: async id => {
+    const url = `/email/domain/${config.domain}/account/${id}`;
 
     try {
       return await ovh.requestPromised('GET', url, {});
@@ -28,14 +28,14 @@ const betaOVH = {
       throw new Error(`OVH Error GET on ${url} : ${JSON.stringify(err)}`);
     }
   },
-  createEmail: async (name, password) => {
+  createEmail: async (id, password) => {
     const url = `/email/domain/${config.domain}/account`;
 
     try {
-      console.log(`OVH POST ${url} name=${name}`);
+      console.log(`OVH POST ${url} name=${id}`);
 
       return await ovh.requestPromised('POST', url, {
-        accountName: name,
+        accountName: id,
         password
       });
     } catch (err) {
