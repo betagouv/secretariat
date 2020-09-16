3b6baf9b26033872115a1bd4a1e4158c2804ba45
Alejandro MG <alejandro@amguillen.dev>
2020-09-16 11:12:33 +0200

    Adds toggle-drawer in mobile view

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,3 +1,11 @@
 (function () {
   "use strict";
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const drawerToggleButton = document.getElementById('drawer-toggle')
+    const drawer = document.getElementById('drawer')
+    drawerToggleButton.addEventListener('click', () => {
+      drawer.classList.toggle('hidden-mobile')
+    })
+  })
 }());
