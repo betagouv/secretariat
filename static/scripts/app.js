b2a6f6fb543815ce8743d1c1d92c97ce06a13768
Alejandro MG <alejandro@amguillen.dev>
2020-09-16 11:36:32 +0200

    Arrow rotates when drawer is extended

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -3,9 +3,11 @@
 
   window.addEventListener('DOMContentLoaded', () => {
     const drawerToggleButton = document.getElementById('drawer-toggle')
+    const drawerToggleArrow = document.getElementById('drawer-toggle-arrow')
     const drawer = document.getElementById('drawer')
     drawerToggleButton.addEventListener('click', () => {
       drawer.classList.toggle('hidden-mobile')
+      drawerToggleArrow.classList.toggle('rotate180', !drawer.classList.contains('hidden-mobile'))
     })
   })
 }());
