66f68490bbd87e9ba25c413fe22af919cbbf34fe
Julien DAUPHANT <jdauphant@users.noreply.github.com>
2020-10-14 22:36:39 +0200

    Update test-account.js

--- tests/test-account.js
+++ tests/test-account.js
@@ -107,7 +107,7 @@ describe("Account", () => {
       knex('marrainage').insert({
         username: 'utilisateur.actif',
         last_onboarder: 'utilisateur.peutimporte',
-        last_updated: (Date.now() - 24*3600*1000 - 5)
+        last_updated: (Date.now() - 24*3600*1000 - 5)/1000 //Data saved in seconds
       }).then(() => {
         chai.request(app)
           .get('/account')
