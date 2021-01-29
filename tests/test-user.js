d1e94d821f5b7e0f77572040d479579d3a63798f
lucharrier <lucharrier@gmail.com>
2021-01-29 14:52:36 +0100

    Fixed case

--- tests/test-user.js
+++ tests/test-user.js
@@ -7,7 +7,7 @@ const utils = require('./utils.js');
 const knex = require('../db');
 const controllerUtils = require('../controllers/utils');
 const { createEmailAddresses } = require('../schedulers/emailCreationScheduler');
-const sendinblue = require('../lib/sendInBlue');
+const sendInBlue = require('../lib/sendInBlue');
 
 describe('User', () => {
   describe('POST /users/:username/email unauthenticated', () => {
@@ -32,7 +32,7 @@ describe('User', () => {
 
     beforeEach((done) => {
       this.sendEmailStub = sinon.stub(controllerUtils, 'sendMail').returns(true);
-      this.sendInBlueAddContactToList = sinon.stub(sendinblue, 'addContactToList').returns(true);
+      this.sendInBlueAddContactToList = sinon.stub(sendInBlue, 'addContactToList').returns(true);
       done();
     });
 
@@ -637,7 +637,7 @@ describe('User', () => {
 
     beforeEach((done) => {
       this.sendEmailStub = sinon.stub(controllerUtils, 'sendMail').returns(true);
-      this.sendInBlueAddContactToList = sinon.stub(sendinblue, 'addContactToList').returns(true);
+      this.sendInBlueAddContactToList = sinon.stub(sendInBlue, 'addContactToList').returns(true);
       done();
     });
 
