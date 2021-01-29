28a9166214d40e9bfcb4982a0c356e26b5866388
Paul Leclercq <paleclercq@gmail.com>
2021-01-29 14:33:44 +0100

    refactor: limit OVH API calls to create email and marrainage (#405)
    * wip(refactor): limit OVH API calls to create email and marrainage
    
    * wip(test): debug unregistered users
    
    * test: get unregistered ovh users
    
    * fix(test): change mocking route to return an array
    
    * test: diff between github and ovh users performed by lodash diff method
    
    * doc: clarify getUnregisteredOVHUsers comments
    
    * test: refactor tests that used another route to get email info
    
    * cleaning code
    
    * log(cron): how many users need an account
    
    * cron(emailCreationJob): every 4 minutes instead of every minute
    
    * chores: clean comment

--- betagouv.js
+++ betagouv.js
@@ -53,6 +53,15 @@ const betaOVH = {
       throw new Error(`OVH Error GET on ${url} : ${JSON.stringify(err)}`);
     }
   },
+  getAllEmailInfos: async () => { // https://eu.api.ovh.com/console/#/email/domain/%7Bdomain%7D/account#GET
+    const url = `/email/domain/${config.domain}/account/`;
+
+    try {
+      return await ovh.requestPromised('GET', url, {});
+    } catch (err) {
+      throw new Error(`OVH Error GET on ${url} : ${JSON.stringify(err)}`);
+    }
+  },
   createEmail: async (id, password) => {
     const url = `/email/domain/${config.domain}/account`;
 
