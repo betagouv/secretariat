d4433c15f065a0bf0a06befc51a36e21c05eecf6
lucharrier <lucharrier@gmail.com>
2021-02-01 12:20:48 +0100

    Added uncomment test

--- tests/test-lib-sendinblue.js
+++ tests/test-lib-sendinblue.js
@@ -7,19 +7,19 @@ const config = require('../config');
 const sib = rewire('../lib/sendInBlue');
 
 describe('Test send in blue lib', () => {
-//   it('Test user info to send in blue format', () => {
-//     const user = testUsers.find((u) => u.id === 'membre.nouveau');
-//     const formatUserInfo = sib.__get__('formatUserInfo');
-//     const formatedData = formatUserInfo(user);
-//     _.isEqual({
-//       NOM_COMPLET: user.fullname,
-//       ROLE: user.missions[0].role,
-//       MISSION_START: user.missions[0].start,
-//       MISSION_END: user.missions[0].end,
-//       MISSION_STATUS: user.missions[0].status,
-//       MISSION_EMPLOYER: user.missions[0].employer,
-//     }, formatedData).should.be.true;
-//   });
+  it('Test user info to send in blue format', () => {
+    const user = testUsers.find((u) => u.id === 'membre.nouveau');
+    const formatUserInfo = sib.__get__('formatUserInfo');
+    const formatedData = formatUserInfo(user);
+    _.isEqual({
+      NOM_COMPLET: user.fullname,
+      ROLE: user.missions[0].role,
+      MISSION_START: user.missions[0].start,
+      MISSION_END: user.missions[0].end,
+      MISSION_STATUS: user.missions[0].status,
+      MISSION_EMPLOYER: user.missions[0].employer,
+    }, formatedData).should.be.true;
+  });
   it('Test send it blue wrapper class call to send in blue api', () => {
     const sendInBlueCall = nock(/.*sendinblue.com/)
     .get(/^.*api\/v3/)
