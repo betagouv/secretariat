024cd71a62706e4b6ac2a7134862f7e8fa700622
lucharrier <lucharrier@gmail.com>
2021-02-15 09:44:37 +0100

    Added pad as a class

--- schedulers/newsletterScheduler.js
+++ schedulers/newsletterScheduler.js
@@ -1,52 +1,10 @@
-require('dotenv').config();
-const fetch = require('node-fetch');
-
-const objectToFormBody = (obj) => {
-  const formBody = [];
-  Object.keys(obj).forEach((property) => {
-    const encodedKey = encodeURIComponent(property);
-    const encodedValue = encodeURIComponent(obj[property]);
-    formBody.push(`${encodedKey}=${encodedValue}`);
-  });
-  return formBody.join('&');
-};
+const PAD = require('../lib/pad');
 
 const createNewNote = async () => {
-  const details = {
-    email: process.env.SECRETARIAT_PAD_USERNAME,
-    password: process.env.SECRETARIAT_PAD_PASSWORD,
-  };
-
-  const formBody = objectToFormBody(details);
-
-  let resp = await fetch('https://pad.incubateur.net');
-
-  resp = await fetch('https://pad.incubateur.net/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-      cookie: resp.headers.get('set-cookie'),
-    },
-    body: formBody,
-  });
-
-  const cookie = resp.headers.get('set-cookie');
-
+  const pad = new PAD();
   const NEWSLETTER_TEMPLATE_ID = 'K9_mewG9SmSvAFjSqHUdHw';
-  const newsletterTemplateContent = await fetch(`https://pad.incubateur.net/${NEWSLETTER_TEMPLATE_ID}/download`, {
-    headers: {
-      cookie,
-    },
-    method: 'GET',
-  }).then((res) => res.text());
-  const result = await fetch('https://pad.incubateur.net/new', {
-    headers: {
-      'Content-Type': 'text/markdown',
-      cookie,
-    },
-    body: newsletterTemplateContent,
-    method: 'POST',
-  });
+  const newsletterTemplateContent = await pad.getNoteWithId(NEWSLETTER_TEMPLATE_ID);
+  const result = await pad.createNewNoteWithContent(newsletterTemplateContent);
   return result;
 };
 
