cb7d34f339ead9d4a27007b4d3c132491ca1daec
lucharrier <lucharrier@gmail.com>
2021-03-12 11:51:29 +0100

    Fixed newsletter id

--- schedulers/newsletterScheduler.js
+++ schedulers/newsletterScheduler.js
@@ -147,8 +147,7 @@ const sendNewsletter = async () => {
 
   if (currentNewsletter) {
     const pad = new HedgedocApi(config.padEmail, config.padPassword, config.padURL);
-    const newsletterContent = await pad.getNotePublishedVersionWithId(currentNewsletter.url.replace(config.padURL, ''));
-    console.log('Will send newsletter content', newsletterContent);
+    const newsletterContent = await pad.getNotePublishedVersionWithId(currentNewsletter.url.replace(`${config.padURL}/`, ''));
     await utils.sendMail(config.newsletterBroadcastList,
       `Infolettre interne de la communauté beta.gouv.fr du ${utils.formatDateToFrenchTextReadableFormat(new Date())}`,
       newsletterContent);
