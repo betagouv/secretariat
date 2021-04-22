08143ac97e44b44390d5a0f8392b5f6cc3c92888
lucharrier <lucharrier@gmail.com>
2021-04-22 12:14:30 +0200

    Added id in seeding data

--- tests/seed/fake_data.js
+++ tests/seed/fake_data.js
@@ -3,17 +3,20 @@ exports.seed = async function (knex) {
   // A few universities
   const newsletterList = [
     {
+      id: 1,
       year_week: '2021-10',
       url: 'https://pad.incubateur.net/infolettre-2021-10-d38417a9',
       created_at: '2021-04-04 00:00:00+00',
     },
     {
+      id: 2,
       year_week: '2021-05',
       url: 'https://pad.incubateur.net/bIl_kERiTAmK6i19N-96RA',
       sent_at: '2021-02-04 00:00:00+00',
       created_at: '2021-02-04 00:00:00+00',
     },
     {
+      id: 3,
       year_week: '2021-06',
       url: 'https://pad.incubateur.net/w61b5DNLScmt7EoZF3FFdQ',
       sent_at: '2021-02-11 00:00:00+00',
@@ -21,6 +24,7 @@ exports.seed = async function (knex) {
       created_at: '2021-02-11 00:00:00+00',
     },
     {
+      id: 4,
       year_week: '2021-07',
       url: 'https://pad.incubateur.net/3xOZWdxSTOGKmN6SYCnMPA',
       sent_at: '2021-02-18 00:00:00+00',
@@ -28,6 +32,7 @@ exports.seed = async function (knex) {
       validator: 'julien.dauphant',
     },
     {
+      id: 5,
       year_week: '2021-08',
       url: 'https://pad.incubateur.net/3b287fH_SUWreI2gBJR58w',
       sent_at: '2021-02-25 00:00:00+00',
