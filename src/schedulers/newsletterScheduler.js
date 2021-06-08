3e19dec4380ff0fa9ce80bb61f3301ccd8200d4d
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-06-08 11:35:57 +0200

    Fix reminders

--- src/schedulers/newsletterScheduler.js
+++ src/schedulers/newsletterScheduler.js
@@ -66,16 +66,14 @@ const computeMessageReminder = (reminder, newsletter) => {
   if (reminder === 'FIRST_REMINDER') {
     message = `*Newsletter interne* :loudspeaker: : voici le pad de la semaine ${newsletter.url}.
       Remplissez le pad avec vos news/annonces/événements qui seront présentées à l'hebdo beta.gouv.
-      Le pad sera envoyé à la communauté vendredi.`;
+      Le contenu du pad sera envoyé jeudi, sous forme d'infolettre à la communauté.`;
   } else if (reminder === 'SECOND_REMINDER') {
     message = `*:wave: Retrouvez nous pour l'hebdo beta.gouv à midi sur http://invites.standup.incubateur.net/*
       Remplissez le pad avec vos news/annonces/événements ${newsletter.url}.
-      Le pad sera envoyé à la communauté vendredi.`;
+      Le pad sera envoyé à 16 h, sous forme d'infolettre à la communauté.`;
   } else {
     message = `*:rolled_up_newspaper: La newsletter va bientôt partir !*
-      Vérifie que le contenu du pad ${newsletter.url} de la newsletter est prêt à être envoyé à la communauté.
-      Elle sera envoyé à 16h.
-    `;
+      Vérifie une dernière fois le contenu du pad ${newsletter.url}. À 16 h, il sera envoyé à la communauté.`;
   }
   return message;
 };
