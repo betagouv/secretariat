52e057b0ec13fa66f7b3a5183a42271892cf36f4
Maxime Dréau <maxime@dreau.fr>
2021-06-30 11:12:05 +0200

    Add a loading bar using buunguyen/topbar
    Close #290

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,8 +1,8 @@
-(function () {
-  "use strict";
+import topbar from 'topbar'
 
-  function initializeDrawerToggle() {
-    const drawerToggleButton = document.getElementById('drawer-toggle')
+function initializeDrawerToggle() {
+  const drawerToggleButton = document.getElementById('drawer-toggle')
+  if (drawerToggleButton) {
     const drawerToggleArrow = document.getElementById('drawer-toggle-arrow')
     const drawer = document.getElementById('drawer')
     drawerToggleButton.addEventListener('click', () => {
@@ -10,8 +10,18 @@
       drawerToggleArrow.classList.toggle('rotate180', !drawer.classList.contains('hidden-mobile'))
     })
   }
+}
 
-  window.addEventListener('DOMContentLoaded', () => {
-    initializeDrawerToggle()
+window.addEventListener('DOMContentLoaded', () => {
+  initializeDrawerToggle()
+})
+
+// show the loading topbar when navigating on desktop only (mobile browsers already have their loading bar)
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+if (!isMobile) {
+  window.addEventListener('beforeunload', () => {
+    setTimeout(() => {
+      topbar.show()
+    }, 500)
   })
-}());
+}
