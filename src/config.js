81b6379b2ad51d90034ce959c65048e37d8c4335
Charrier <lucas.charrier@beta.gouv.fr>
2021-07-06 17:21:09 +0200

    Ajouter le lien de connexion à mattermost (#697)
    * Added changed onboarding email to add mattermost instead of slack
    
    * Change l'adresse de connexion a mattermost dans l'email
    
    * Update views/emails/createEmail.ejs
    
    Co-authored-by: Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>
    
    Co-authored-by: Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>

--- src/config.js
+++ src/config.js
@@ -45,5 +45,6 @@ module.exports = {
   sentryDNS: process.env.SENTRY_DNS || false,
   mattermostBotToken: process.env.MATTERMOST_BOT_TOKEN,
   mattermostTeamId: process.env.MATTERMOST_TEAM_ID || 'testteam',
+  mattermostInvitationLink: process.env.MATTERMOST_INVITATION_LINK || '',
   investigationReportsIframeURL: process.env.INVESTIGATION_REPORTS_IFRAME_URL || '',
 };
