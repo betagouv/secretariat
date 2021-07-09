c088429a9043ea634861b07eb71cf7b24fb4d402
lucharrier <lucharrier@gmail.com>
2021-07-09 17:26:29 +0200

    Added files

--- src/schedulers/cron.js
+++ src/schedulers/cron.js
@@ -1,3 +1,13 @@
 require('./marrainageScheduler');
 require('./emailCreationScheduler');
 require('./newsletterScheduler');
+const { CronJob } = require('cron');
+const { addGithubUserToOrganization } = require('./githubScheduler');
+
+module.exports.addGithubUserToOrganization = new CronJob(
+  '0 */4 * * * 1-5',
+  addGithubUserToOrganization,
+  null,
+  true,
+  'Europe/Paris',
+);
