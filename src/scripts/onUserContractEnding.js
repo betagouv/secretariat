ea33f39f1322db1267dbeba219fddbc51749ef60
lucharrier <lucharrier@gmail.com>
2021-07-15 17:55:19 +0200

    Fixed args for script

--- src/scripts/onUserContractEnding.js
+++ src/scripts/onUserContractEnding.js
@@ -1,4 +1,7 @@
 import { onUserContractEnding } from '../schedulers/userContractEndingScheduler';
 
 const args = process.argv.slice(2);
+if (args[1]) {
+  args[1] = JSON.parse(args[1]);
+}
 onUserContractEnding(...args);
