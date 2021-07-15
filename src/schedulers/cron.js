a042e71d169e22fe1dc663b4049587e6c23b419d
lucharrier <lucharrier@gmail.com>
2021-07-15 17:29:45 +0200

    Comment cron

--- src/schedulers/cron.js
+++ src/schedulers/cron.js
@@ -2,21 +2,21 @@ require('./marrainageScheduler');
 require('./emailCreationScheduler');
 require('./newsletterScheduler');
 
-const { CronJob } = require('cron');
-const { onUserContractEnding } = require('./userContractEndingScheduler');
+// const { CronJob } = require('cron');
+// const { onUserContractEnding } = require('./userContractEndingScheduler');
 
-const onUserContractEndIn15days = new CronJob(
-  '0 */8 * * * *',
-  () => onUserContractEnding('mail15days'),
-  null,
-  true,
-  'Europe/Paris',
-);
+// const onUserContractEndIn15days = new CronJob(
+//   '0 */8 * * * *',
+//   () => onUserContractEnding('mail15days'),
+//   null,
+//   true,
+//   'Europe/Paris',
+// );
 
-const onUserContractEndIn2days = new CronJob(
-  '0 */8 * * * *',
-  () => onUserContractEnding('mail2days'),
-  null,
-  true,
-  'Europe/Paris',
-);
+// const onUserContractEndIn2days = new CronJob(
+//   '0 */8 * * * *',
+//   () => onUserContractEnding('mail2days'),
+//   null,
+//   true,
+//   'Europe/Paris',
+// );
