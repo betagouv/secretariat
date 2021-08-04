8116af09c045f040f42cff671c78eab5f57ee197
lucharrier <lucharrier@gmail.com>
2021-08-04 10:42:45 +0200

    Fixed typescript

--- src/scripts/sendJ1Email.ts
+++ src/scripts/sendJ1Email.ts
@@ -1,7 +1,8 @@
-const { sendJ1Email } = require('../schedulers/userContractEndingScheduler');
+import { sendJ1Email } from '../schedulers/userContractEndingScheduler';
 
 const args = process.argv.slice(2);
 if (args[1]) {
   args[1] = JSON.parse(args[1]);
 }
+// @ts-ignore
 sendJ1Email(...args);
--- src/scripts/sendJ30Email.ts
+++ src/scripts/sendJ30Email.ts
@@ -1,7 +1,8 @@
-const { sendJ30Email } = require('../schedulers/userContractEndingScheduler');
+import { sendJ30Email } from '../schedulers/userContractEndingScheduler';
 
 const args = process.argv.slice(2);
 if (args[1]) {
   args[1] = JSON.parse(args[1]);
 }
+// @ts-ignore
 sendJ30Email(...args);
