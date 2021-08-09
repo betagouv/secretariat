97acdb4c447a39b7648a683a84a87930aff6f5a1
Calev Eliacheff <c.eliacheff@gmail.com>
2021-08-09 10:38:49 +0200

    fixed TS commands available in CI

--- tests/seed/fake_data.ts
+++ tests/seed/fake_data.ts
@@ -1,23 +1,23 @@
-import utils from '../utils';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function seed(knex) {
   // A few universities
   const newsletterList = [
     {
-      id: utils.randomUuid(),
+      id: uuidv4(),
       year_week: '2021-10',
       url: 'https://pad.incubateur.net/infolettre-2021-10-d38417a9',
       created_at: '2021-04-04 00:00:00+00',
     },
     {
-      id: utils.randomUuid(),
+      id: uuidv4(),
       year_week: '2021-05',
       url: 'https://pad.incubateur.net/bIl_kERiTAmK6i19N-96RA',
       sent_at: '2021-02-04 00:00:00+00',
       created_at: '2021-02-04 00:00:00+00',
     },
     {
-      id: utils.randomUuid(),
+      id: uuidv4(),
       year_week: '2021-06',
       url: 'https://pad.incubateur.net/w61b5DNLScmt7EoZF3FFdQ',
       sent_at: '2021-02-11 00:00:00+00',
@@ -25,7 +25,7 @@ export async function seed(knex) {
       created_at: '2021-02-11 00:00:00+00',
     },
     {
-      id: utils.randomUuid(),
+      id: uuidv4(),
       year_week: '2021-07',
       url: 'https://pad.incubateur.net/3xOZWdxSTOGKmN6SYCnMPA',
       sent_at: '2021-02-18 00:00:00+00',
@@ -33,7 +33,7 @@ export async function seed(knex) {
       validator: 'julien.dauphant',
     },
     {
-      id: utils.randomUuid(),
+      id: uuidv4(),
       year_week: '2021-08',
       url: 'https://pad.incubateur.net/3b287fH_SUWreI2gBJR58w',
       sent_at: '2021-02-25 00:00:00+00',
