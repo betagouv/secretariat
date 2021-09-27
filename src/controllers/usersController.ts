b52988318b5c07a13f036b20e97b2708660e773c
lucharrier <lucharrier@gmail.com>
2021-09-27 16:42:22 +0200

    Fix quand l'utilisateur n'existe pas

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -330,7 +330,7 @@ export async function manageSecondaryEmailForUser(req, res) {
         action_on_username: username,
         action_metadata: {
           value: secondaryEmail,
-          old_value: dbUser.secondary_email,
+          old_value: dbUser ? dbUser.secondary_email : null,
         }
       })
       req.flash('message', 'Ton compte email secondaire a bien mis à jour.');
