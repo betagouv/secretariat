12fc6f10e42b49b51ec4d3dbdb8f433d7d30a84e
Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>
2021-10-18 13:42:58 +0200

    Update src/controllers/usersController.ts

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -254,7 +254,7 @@ export async function deleteEmailForUser(req, res) {
       console.log(`Suppression des redirections de l'email de ${username} (à la demande de ${req.user.id})`);
     }
 
-    await BetaGouv.createRedirection(`${username}@${domain}`, config.leavesEmail, false);
+    await BetaGouv.createRedirection(utils.buildBetaEmail(username), config.leavesEmail, false);
     await knex('users')
       .update({ secondary_email: null })
       .where({ username });
