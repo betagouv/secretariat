54d5c5d2ab164e64dc6eadb4704861e7f4ca5c76
Charrier <lucas.charrier@beta.gouv.fr>
2021-10-26 15:07:02 +0200

    Ajout des logs d'actions (#852)
    * Added events
    
    * Added new log event
    
    * Update log event description
    
    * Fixed tests
    
    * Update events
    
    * Description mise a jour
    
    * Fix typo
    
    * Fixed marrainage creation logged event
    
    * Ajoute hstore column
    
    * Ajoute fichier hstore parse et stringify
    
    * Ajoute des variables non declarees
    
    * Fixed migration
    
    * Fixed migration
    
    * Fixed query previous secondary email
    
    * Fix typo
    
    * Fix quand l'utilisateur n'existe pas
    
    Co-authored-by: Alejandro M Guillén <alejandro@amguillen.dev>

--- src/controllers/marrainageController.ts
+++ src/controllers/marrainageController.ts
@@ -4,6 +4,7 @@ import config from '../config';
 import BetaGouv from '../betagouv';
 import * as utils from './utils';
 import knex from '../db';
+import { addEvent, EventCode } from '../lib/events'
 
 async function selectRandomOnboarder(newcomerId) {
   const users = await BetaGouv.usersInfos();
@@ -178,6 +179,10 @@ export async function createRequest(req, res) {
     console.log(
       `Marrainage crée à la demande de ${user.id} pour ${newcomer.id}. Marrain·e selectionné·e : ${onboarder.id}`
     );
+    addEvent(EventCode.MARRAINAGE_CREATED, {
+      created_by_username: user.id,
+      action_on_username: newcomer.id
+    })
 
     if (newcomer.id === req.user.id)
       req.flash(
@@ -223,7 +228,10 @@ export async function acceptRequest(req, res) {
     await knex('marrainage')
       .where({ username: newcomer.id })
       .update({ completed: true, last_updated: knex.fn.now() });
-
+    addEvent(EventCode.MARRAINAGE_ACCEPTED, {
+      created_by_username: onboarder.id,
+      action_on_username: onboarder.id
+    })
     const html = await ejs.renderFile('./views/emails/marrainageAccept.ejs', {
       newcomer,
       onboarder,
@@ -272,7 +280,10 @@ export async function declineRequest(req, res) {
         last_onboarder: declinedOnboarder.id,
         completed: false,
       });
-
+    addEvent(EventCode.MEMBER_MARRAINAGE_DECLINED, {
+      created_by_username: declinedOnboarder.id,
+      action_on_username: declinedOnboarder.id
+    })
     if (marrainageDetailsReponse.length !== 1) {
       console.log(
         `Marrainage refusé non existant pour ${newcomer.id}. Marrain·e indiqué·e : ${declinedOnboarder.id}`
