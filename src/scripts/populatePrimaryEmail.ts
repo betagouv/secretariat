778eb0142006a367528e71a08a4ef3aa67a3ea9b
lucharrier <lucharrier@gmail.com>
2021-11-29 12:10:37 +0100

    Update script

--- src/scripts/populatePrimaryEmail.ts
+++ src/scripts/populatePrimaryEmail.ts
@@ -4,13 +4,13 @@ import knex from '../db';
 
 const populatePrimaryEmail = async() => {
     const allOvhEmails = await Betagouv.getAllEmailInfos();
-    allOvhEmails.forEach(async emailId => {
+    for (const emailId of allOvhEmails) {
         await knex('users').insert({
             username: emailId,
             primary_email: buildBetaEmail(emailId)
         }).onConflict('username')
         .merge();
-    });
+    }
 }
 
 populatePrimaryEmail()
