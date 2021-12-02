55e636ba4ff03d0e4d6a20c0e6258fedd5706670
lucharrier <lucharrier@gmail.com>
2021-12-02 17:00:15 +0100

    Added mattermost url in config

--- src/config.ts
+++ src/config.ts
@@ -38,6 +38,7 @@ export default {
   newsletterCronTime: process.env.NEWSLETTER_CRON_TIME || '0 4 * * 5',
   newsletterHashSecret: process.env.NEWSLETTER_HASH_SECRET,
   newsletterSendTime: process.env.NEWSLETTER_SEND_TIME,
+  mattermostURL: process.env.MATTERMOST_URL,
   senderEmail: process.env.MAIL_SENDER || 'secretariat@incubateur.net',
   slackWebhookURLSecretariat: process.env.SLACK_WEBHOOK_URL_SECRETARIAT,
   slackWebhookURLGeneral: process.env.SLACK_WEBHOOK_URL_GENERAL,
