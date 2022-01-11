a10932e39e8821506113b3924fb4c0a662995ba2
Charrier <lucas.charrier@beta.gouv.fr>
2022-01-11 17:30:22 +0100

    Update hashtoparam.js

--- static/scripts/hashtoparam.js
+++ static/scripts/hashtoparam.js
@@ -3,8 +3,7 @@
 
     const search = new URLSearchParams(window.location.search.replace('?', ''))
     const hash = window.location.hash.replace('#', '')
-    // scroll main view to anchors, wish does not work otherwise because scroll must be done in
-    // main container not on window.
+    // set hash to query params to use it after user logged in
     if (hash && window.location.pathname === '/login' && search.get('next')) {
         search.set('anchor', hash)
         window.location.search = '?' + search.toString();
