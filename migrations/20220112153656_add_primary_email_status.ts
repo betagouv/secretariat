96566fe28f45767d76a0fdd78fc17090fa970478
lucharrier <lucharrier@gmail.com>
2022-01-19 16:10:12 +0100

    Added default value to column primary_email_status in migration

--- migrations/20220112153656_add_primary_email_status.ts
+++ migrations/20220112153656_add_primary_email_status.ts
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
   return knex.schema.table('users', (table) => {
-    table.text('primary_email_status');
+    table.text('primary_email_status').defaultTo('EMAIL_ACTIVE');
     table.datetime('primary_email_status_updated_at').defaultTo(knex.fn.now());
   });
 };
@@ -8,6 +8,6 @@ exports.up = function(knex) {
 exports.down = function(knex) {
   return knex.schema.table('users', (table) => {
     table.dropColumn('primary_email_status');
-    table.datetime('primary_email_status_updated_at');
+    table.dropColumn('primary_email_status_updated_at');
   });
 };
