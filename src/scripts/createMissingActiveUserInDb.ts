521e1bf1c1c1f5984f17c2ef077a74f7b89dd8dd
Charrier <lucas.charrier@beta.gouv.fr>
2022-01-25 11:38:46 +0100

    Added script create missing active user (#1058)

---
+++ src/scripts/createMissingActiveUserInDb.ts
@@ -0,0 +1,24 @@
+import { buildBetaEmail } from '../controllers/utils';
+import Betagouv from '../betagouv'
+import knex from '../db';
+import { Member } from '../models/member';
+
+const createMissingActiveUserInDb = async() => {
+    const activeUsers: Member[] = await Betagouv.getActiveRegisteredOVHUsers();
+    for (const user of activeUsers) {
+        try {
+            const users = await knex('users').where({ username: user.id }).first()
+            if (!users.length) {
+                console.log(`User ${user.id} is active but not in bdd`)
+                await knex('users').insert({
+                    username: user.id,
+                    primary_email: buildBetaEmail(user.id)
+                })
+            }
+        } catch (e) {
+            // error
+        }
+    }
+}
+
+createMissingActiveUserInDb()
