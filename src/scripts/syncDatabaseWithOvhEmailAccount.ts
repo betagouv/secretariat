692e2ae10a25c27d8d4a33cb7c5917933551d25b
Charrier <lucas.charrier@beta.gouv.fr>
2022-02-14 09:44:48 +0100

    Added sync database and ovh email account (#1116)
    * Added sync database and ovh email account
    
    * Removed useless imports

---
+++ src/scripts/syncDatabaseWithOvhEmailAccount.ts
@@ -0,0 +1,27 @@
+import betagouv from "../betagouv";
+import knex from "../db";
+import { EmailStatusCode } from "../models/dbUser";
+import { Member } from "../models/member";
+
+const syncDatabaseWithOvhEmailAccount = async () => {
+    const users : Member[] = await betagouv.usersInfos();
+    const usernames : string[] = users.map(user => user.id);
+    const ovhAccounts : string[] = await betagouv.accounts();
+    const usernamesWithoutOvhEmailAccount = usernames.filter(
+        username => !ovhAccounts.includes(username));
+    console.log('Will set primary_email_status to deleted for users :', usernamesWithoutOvhEmailAccount)
+    if (process.env.APPLY_SYNC_DATABASE_WITH_OVH_EMAIL_ACOUNT) {
+        await knex('users')
+            .whereIn(
+            'username', usernamesWithoutOvhEmailAccount
+            )
+            .andWhere({
+                primary_email_status: EmailStatusCode.EMAIL_ACTIVE
+            })
+            .update({
+                primary_email_status: EmailStatusCode.EMAIL_DELETED
+            })
+    }
+}
+
+syncDatabaseWithOvhEmailAccount()
