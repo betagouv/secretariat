4c65a82d78cd78bac9c0e9fb1632bbe9a133e531
Charrier <lucas.charrier@beta.gouv.fr>
2022-03-02 14:15:39 +0100

    Update checkMfa.ts (#1151)

--- src/scripts/checkMfa.ts
+++ src/scripts/checkMfa.ts
@@ -1,7 +1,9 @@
 import { getUserWithParams, MattermostUser } from "../lib/mattermost";
 
 const checkMFA = async() => {
-    const users : MattermostUser[] = await getUserWithParams()
+    const users : MattermostUser[] = await getUserWithParams({
+        active: true
+    })
     const countMFA = users.filter(user => user.mfa_active).length;
     console.info(`users with mfa_active : ${countMFA} on ${users.length}`)
     console.info(`users with mfa_active percentage : ${(countMFA/users.length)/100}%`)
