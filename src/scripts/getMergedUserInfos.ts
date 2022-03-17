74d0176745e83c6f4c2450278053770c22ce9adf
Charrier <lucas.charrier@beta.gouv.fr>
2022-03-17 13:06:04 +0100

    Update getMergedUserInfos.ts

--- src/scripts/getMergedUserInfos.ts
+++ src/scripts/getMergedUserInfos.ts
@@ -1,9 +1,12 @@
 import betagouv from "../betagouv";
-import { Member } from "../models/member"
 import knex from "../db";
+import { checkUserIsExpired } from "../controllers/utils";
 
 const getIntraUsersEmails = async () => {
-    const members : Member[] = await betagouv.getActiveRegisteredOVHUsers()
+    const users = await betagouv.usersInfos();
+    const members = users.filter(
+      (user) => !checkUserIsExpired(user)
+    );
     const intras = members.filter(member => member.domaine === 'Intraprenariat')
     const intraDBUsers = await knex('users').whereIn('username', intras.map(intra => intra.id))
     intraDBUsers.forEach(user => {
