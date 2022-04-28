473cf9cf5482a7da288149288a0dc09f0fb1dcdb
lucharrier <lucharrier@gmail.com>
2022-04-28 11:23:01 +0200

    Ajoute nouveau script pour demande d'update d'info

---
+++ src/scripts/sendMessageUpdateInfoToUsers.ts
@@ -0,0 +1,77 @@
+import ejs from 'ejs';
+import BetaGouv from '../betagouv';
+import * as utils from '../controllers/utils';
+import knex from '../db';
+import { DBUser, GenderCode } from '../models/dbUser';
+import { Member, MemberWithEmailsAndMattermostUsername } from '../models/member';
+import * as mattermost from '../lib/mattermost';
+import { fetchCommuneDetails } from 'src/lib/searchCommune';
+
+export async function sendMessageToUpdateInfoToAllUsers() {
+    const allMattermostUsers = await mattermost.getUserWithParams();
+    const allMattermostUsersEmails = allMattermostUsers.map(
+        (mattermostUser) => mattermostUser.email
+      );
+    const users: Member[] = await BetaGouv.usersInfos();
+    const activeUsers = users.filter((user) => !utils.checkUserIsExpired(user));
+    const concernedUsers: DBUser[] = await knex('users')
+      .whereIn(
+        'username',
+        activeUsers.map((user) => user.id)
+      );
+    
+    const concernedUserWithMattermostUsers : (MemberWithEmailsAndMattermostUsername & DBUser)[] = concernedUsers.map(
+        (user) => {
+            const index = allMattermostUsersEmails.indexOf(
+              user.primary_email
+            );
+            const githubUser = activeUsers.find(ghUser => ghUser.id === user.username)
+            return {
+                ...githubUser,
+                ...user,
+                primary_email: user.primary_email,
+                secondary_email: user.secondary_email,
+                mattermostUsername: index > -1 ? allMattermostUsers[index].username : undefined,
+            };
+          }
+    );
+    
+    for (const user of concernedUserWithMattermostUsers) {
+        if (user.mattermostUsername === 'lucas.charrier') {
+            try {
+                const secretariatUrl = `https://secretariat.incubateur.net/`;
+                const messageContent = await ejs.renderFile(
+                    `./src/views/templates/emails/updateUserInfoEmail.ejs`,
+                    {
+                        secretariatUrl,
+                        user: {
+                            ...user,
+                            tjm: user.tjm || 'Non défini',
+                            gender: GenderCode[user.gender],
+                            legal_status: user.legal_status || 'Non défini',
+                            workplace_insee_code: user.workplace_insee_code ? await fetchCommuneDetails(user.workplace_insee_code) : 'Non défini',
+                            secondary_email: user.secondary_email || 'Non défini'
+                        }
+                    }
+                );
+                console.log(`Message d'update des info utilisateur envoyé à ${user.mattermostUsername}`)
+                await BetaGouv.sendInfoToChat(
+                    messageContent,
+                    'secretariat',
+                    user.mattermostUsername
+                );
+                await sleep(1000);
+            } catch (e) {
+                console.log(`Erreur lors de l'envoie à ${user.mattermostUsername}`, e)
+            }
+        }
+    }
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+sendMessageToUpdateInfoToAllUsers()
