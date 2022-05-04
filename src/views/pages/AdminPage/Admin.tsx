61d80f62d108ae1645080fdafe7d5bd31165b9f8
lucharrier <lucharrier@gmail.com>
2022-05-04 11:54:47 +0200

    Added page

---
+++ src/views/pages/AdminPage/Admin.tsx
@@ -0,0 +1,110 @@
+import type { Request } from 'express'
+import React, { ReactNode } from 'react'
+import { InnerPageLayout } from '../components/InnerPageLayout'
+import { hydrateOnClient } from '../../../lib/hydrateOnClient'
+
+interface Email {
+  github?: string,
+  expired: boolean,
+  account: string,
+  endDate: string,
+  startDate: string,
+  id: string,
+  email: string,
+  redirections: string[],
+}
+
+interface Props {
+  emails: Email[],
+  expiredEmails: Email[],
+  request: Request,
+  title: string,
+  currentUserId: string,
+  errors: string[],
+  messages: string[],
+}
+
+/* Pure component */
+export const Admin = InnerPageLayout(function (props: Props) {
+
+  const rows : ReactNode[] = props.emails.map(email => {
+    return (<tr>
+        <td>
+            <a href={`/community/${email.id}`}>{email.email}</a>
+            <div data-id={email.id} className="redirection-list">
+                { email.redirections.map(function(redirection) {
+                  return <>{redirection}<br /></>
+                })}
+            </div>
+        </td>
+        <td>{email.github ? <a href={`https://github.com/betagouv/beta.gouv.fr/blob/master/content/_authors/${email.id}.md`} target="_blank">✔ Oui</a> : '❌ Non !' }</td>
+        <td>{!email.expired ? `✔ Oui` : `❌ Non !` }</td>
+        <td>{email.account ? `✔ Oui` : `❌ Non !` }</td>
+    </tr>)
+  })
+
+  const expiredRows : ReactNode[] = props.expiredEmails.map(function(email) {
+    return (<tr>
+      <td><a href={`/community/${email.id}`}>{email.email}</a>
+            <div data-id={`/community/${email.id}`} className="redirection-list">
+                { email.redirections.map(function(redirection) {
+                  return <>{redirection}<br /></>
+                })}
+            </div>
+        </td>
+        <td>{email.endDate ? email.endDate : '❌ Non !'}</td>
+        <td>{email.account ? '✔ Oui' : '❌ Non !' }</td>
+        <td>{email.github ? <a href={`https://github.com/betagouv/beta.gouv.fr/blob/master/content/_authors/${email.id}.md`} target="_blank">✔ Oui</a> : '❌ Non !' }</td>
+    </tr>)
+  })
+
+  return (
+    <>
+      <div className="module">
+          <div className="panel panel-full-width" id="all">
+              <h3>
+                  Liste des emails
+              </h3>
+              <a href="#expired">⬇️ Voir les comptes expirés</a>
+              <hr />
+              <table className="sortable">
+                  <thead>
+                      <tr>
+                          <th>Email</th>
+                          <th>Fiche sur Github</th>
+                          <th>Date de fin OK</th>
+                          <th>Compte Email</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {rows}
+                  </tbody>
+              </table>
+          </div>
+
+          <div className="panel panel-full-width" id="expired">
+              <h3>
+                  Comptes expirés
+              </h3>
+              <a href="#all">⬆️ Voir tous les comptes</a>
+              <hr />
+              <table className="sortable">
+                  <thead>
+                      <tr>
+                          <th>Email</th>
+                          <th>Date de fin</th>
+                          <th>Compte Email</th>
+                          <th>Fiche sur Github</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {expiredRows}
+                  </tbody>
+              </table>
+          </div>
+      </div>
+    </>
+  )
+})
+
+hydrateOnClient(Admin)
---
+++ src/views/pages/components/InnerPageLayout.tsx
@@ -0,0 +1,121 @@
+
+   
+import type { Request } from 'express'
+import React from 'react'
+
+interface HasRequest {
+  request: Request,
+  title: string,
+  currentUserId: string,
+  errors: string[],
+  messages: string[],
+}
+
+export const InnerPageLayout = <T extends HasRequest>(Component: (props: T) => JSX.Element) => (
+  props: T
+) => {
+  return (
+    <>
+        <html>
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>{ props.title }</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png"/>
+            <link rel="manifest" href="/static/favicon/site.webmanifest"/>
+            <link rel="stylesheet" href="/static/css/main.css"/>
+        </head>
+
+        <body>
+            <div id="dashboard" className="dashboard">
+                <aside className="side-menu" role="navigation" id="navigation">
+                    <button id="drawer-toggle" className="button-outline small primary">Menu <span id="drawer-toggle-arrow">▼</span></button>
+                    <h4 id="logo">🤖&nbsp;Secrétariat</h4>
+                    <ul className="hidden-mobile margin-bottom-5" id="drawer">
+                        <li>
+                            <a href="/account" id="account" 
+                                className="nav-item <% if(activeTab === 'account') { %> active <% } %>">
+                                Mon compte
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/community" id="community"
+                                className="nav-item <% if(activeTab === 'community') { %> active <% } %>">
+                                Communauté
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/startups" id="startups"
+                                className="nav-item <% if(activeTab === 'startups') { %> active <% } %>">
+                                Startups
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/admin" id="admin" 
+                                className="nav-item <% if(activeTab === 'admin') { %> active <% } %>">
+                                Administration
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/newsletters" id="newsletter" 
+                                className="nav-item <% if(activeTab === 'newsletter') { %> active <% } %>">
+                                Infolettres internes
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/resources" id="resources"
+                                className="nav-item <% if(activeTab === 'resources') { %> active <% } %>">
+                                Ressources
+                            </a>
+                        </li>
+                        <li className="nav-end">
+                            <hr />
+                            { (props.currentUserId) &&
+                            <div>
+                                Identifié·e en tant que<br />
+                                <span className="font-weight-bold">{ props.currentUserId }</span>
+                            </div>}
+                            <hr />
+                            <a href="/logout">Se déconnecter</a>
+                            <hr />
+                            <img src="/static/images/betagouv.png" width="150" alt=""/>
+                            <a href="https://github.com/betagouv/secretariat/" target="_blank"
+                                className="text-size-caption text-color-darker-grey">Code source du sécretariat</a>
+                        </li>
+                    </ul>
+                </aside>
+                <main role="main" className="main">
+                    { props.errors.length && 
+                        <div className="notification error">
+                            <strong>Erreur : </strong>
+                            {props.errors.map((message) => {
+                                { message }
+                            })}
+                        </div>
+                    }
+                    { props.messages.length && <div className="notification">
+                        {props.messages.map((message) => {
+                            return <p>{message}</p>
+                        })}
+                        </div>
+                    }
+                    <section className="section section-grey no-padding">
+                        <Component {...props} />
+                        <div className="main-end">
+                            <div>
+                                Identifié·e en tant que<br />
+                                <span className="font-weight-bold">{ props.currentUserId }</span>
+                            </div>
+                            <a href="/logout">Se déconnecter</a>
+                        </div>
+                    </section>
+                </main>
+                </div>
+            </body>
+        </html>
+    </>
+  )
+}
