99050b2cfbad8ed8b64e681c595da5d09fe51a8e
Charrier <lucas.charrier@beta.gouv.fr>
2022-06-07 17:45:13 +0200

    Ajoute script pour publier les offres sur mattermost (#1352)

--- src/schedulers/syncBetagouvAPIScheduler.ts
+++ src/schedulers/syncBetagouvAPIScheduler.ts
@@ -2,9 +2,9 @@ import { checkUserIsExpired } from '../controllers/utils';
 import BetaGouv from '../betagouv';
 import db from '../db';
 import { Domaine, Member } from '../models/member';
-import { Job } from '../models/job';
 import { makeHtmlEmail } from '../views/index.html';
 import { JobMessage } from '../views/emails/jobMessage';
+import { Job } from 'src/models/job';
 
 export const JobMessageHtml = (props: Parameters<typeof JobMessageHtml>[0]) =>
   makeHtmlEmail({
@@ -24,10 +24,12 @@ export async function syncBetagouvUserAPI() {
   }
 }
 
-export async function publishJobsToMattermost() {
-  let jobs : Job[] = await BetaGouv.getJobs()
-  const now = new Date();
-  jobs = jobs.filter(job => new Date(job.published) > now)
+export async function publishJobsToMattermost(jobs) {
+  if (!jobs) {
+    jobs = await BetaGouv.getJobs() as Job[]
+    const now = new Date();
+    jobs = jobs.filter(job => new Date(job.published) > now)
+  }
   for (const domaine of Object.values(Domaine)) {
     const jobsForDomaine = jobs.filter(job => (job.domaines || []).includes(domaine))
     const jobMessage = JobMessageHtml({
---
+++ src/scripts/pubishJobsToMattermost.ts
@@ -0,0 +1,10 @@
+import { publishJobsToMattermost } from "../schedulers/syncBetagouvAPIScheduler";
+
+const args = process.argv.slice(2);
+if (args[0]) {
+  args[0] = JSON.parse(args[0]);
+}
+
+publishJobsToMattermost(args[0]).then(d => {
+    console.log('Finished publish jobs to mattermost')
+})
