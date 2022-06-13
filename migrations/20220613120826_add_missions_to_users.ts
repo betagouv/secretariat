baf9126c5e7c5c5b06dc94dc9bdb41cc91682bd8
lucharrier <lucharrier@gmail.com>
2022-06-13 14:23:13 +0200

    Added migration

---
+++ migrations/20220613120826_add_missions_to_users.ts
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('users', (table) => {
+        table.jsonb('missions')
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table('users', (table) => {
+        table.dropColumn('missions');
+    });
+};
