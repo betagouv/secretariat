c210d3864d93e1db6e190bb502b4aa8410964d44
lucharrier <lucharrier@gmail.com>
2022-06-17 14:46:57 +0200

    Added migration file

---
+++ migrations/20220617123852_add_community_table.ts
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('community', (table) => {
+      table.text('hash').primary();
+      table.text('gender').defaultTo('NSP');
+      table.integer('tjm');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    knex.schema.dropTable('community')
+};
