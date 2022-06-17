7dba88aee67dbff8f4cd455f3c5fc9c24a1bdfc0
lucharrier <lucharrier@gmail.com>
2022-06-17 14:53:37 +0200

    Added user details

--- migrations/20220617123852_add_community_table.ts
+++ migrations/20220617123852_add_community_table.ts
@@ -4,7 +4,7 @@
  */
 exports.up = function(knex) {
     return knex.schema
-    .createTable('community', (table) => {
+    .createTable('user_details', (table) => {
       table.text('hash').primary();
       table.text('gender').defaultTo('NSP');
       table.integer('tjm');
@@ -16,5 +16,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-    knex.schema.dropTable('community')
+    knex.schema.dropTable('user_details')
 };
