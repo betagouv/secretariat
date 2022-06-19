ef34f8b2e0c5c5cadcbd207803f757d546bbeca7
lucharrier <lucharrier@gmail.com>
2022-06-19 15:06:00 +0200

    Update migration

--- migrations/20220619122922_data_migration.ts
+++ migrations/20220619122922_data_migration.ts
@@ -1,13 +1,11 @@
+import crypto from 'crypto';
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
- */
-
-import crypto from 'crypto';
-import { DBUser } from '../src/models/dbUser'
-
+*/
 exports.up = async function(knex) {
-    const users: DBUser[] = await knex('users')
+    const users = await knex('users')
     const salt : string = process.env.HASH_SALT as string
     for (const user of users) {
         var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
