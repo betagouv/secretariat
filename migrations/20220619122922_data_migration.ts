fc5da12063472ea6ba13f23039d2e97a896a5cb6
lucharrier <lucharrier@gmail.com>
2022-06-19 15:10:18 +0200

    Removed types

--- migrations/20220619122922_data_migration.ts
+++ migrations/20220619122922_data_migration.ts
@@ -6,7 +6,7 @@ import crypto from 'crypto';
 */
 exports.up = async function(knex) {
     const users = await knex('users')
-    const salt : string = process.env.HASH_SALT as string
+    const salt = process.env.HASH_SALT
     for (const user of users) {
         var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
         hash.update(user.username);
