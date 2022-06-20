ff8b146c060e320cdb022654d0be77a9ecbe5dd8
Charrier <lucas.charrier@beta.gouv.fr>
2022-06-20 18:08:25 +0200

    Ajoute scrit to post event on mattermost (#1431)
    * Ajoute scrit to post event on mattermost
    
    * Update CommuneSelect.tsx
    
    * Update eventMessage.ejs
    
    * Removed unused variable
    
    * Fixed date format

---
+++ src/schedulers/calendarScheduler.ts
@@ -0,0 +1,23 @@
+import ejs from 'ejs'
+import { getEventsForCalendarFromDateToDate } from "../lib/icalhelper"
+import betagouv from '../betagouv'
+import * as utils from '../controllers/utils';
+
+export const postEventsOnMattermost = async () => {
+    const today = new Date()
+    const dayInSevenDays = new Date()
+    dayInSevenDays.setDate(today.getDate() + 7)
+    const calendarURL = process.env.CALENDAR_URL
+    const events = await getEventsForCalendarFromDateToDate(calendarURL, today, dayInSevenDays)
+    const readableEvents = events.map(event => ({
+        startDate: utils.formatDateToFrenchTextReadableFormat(event.startDate),
+        endDate: utils.formatDateToFrenchTextReadableFormat(event.endDate),
+        location: event.location,
+        title: event.title
+    }))
+    const messageContent = await ejs.renderFile('./src/views/templates/emails/eventMessage.ejs', {
+        events: readableEvents
+    });
+    await betagouv.sendInfoToChat(messageContent, 'incubateur-embauche-autre');
+}
+
