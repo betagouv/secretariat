fcd8b5043f7256b24551ba046aa319c20bbffa37
Charrier <lucas.charrier@beta.gouv.fr>
2022-06-20 17:41:04 +0200

    Update CommuneSelect.tsx

--- src/views/pages/components/CommuneSelect.tsx
+++ src/views/pages/components/CommuneSelect.tsx
@@ -3,14 +3,13 @@ import AsyncSelect from 'react-select/async';
 
 import { ClientOnly } from "./ClientOnly"
 
-export default ({ loadOptions, defaultValue, onChange, placeholder}) => {
+export default ({ loadOptions, defaultValue, onChange, placeholder }) => {
     return <ClientOnly>
         <AsyncSelect
             cacheOptions
             loadOptions={loadOptions}
             defaultOptions
             placeholder={placeholder}
-            onBlur={() => {}}
             defaultInputValue={defaultValue}
             defaultValue={defaultValue}
             onChange={onChange}
