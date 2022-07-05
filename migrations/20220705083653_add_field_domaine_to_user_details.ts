faa290309b156cbf7742752945073270e10a5f71
Charrier <lucas.charrier@beta.gouv.fr>
2022-07-05 10:48:23 +0200

    fix(migration): added missing field domaine to user details (#1468)
    * fix(migration): added missing field domaine to user details
    
    * Update 20220705083653_add_field_domaine_to_user_details.ts

---
+++ migrations/20220705083653_add_field_domaine_to_user_details.ts
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    return knex.schema.table('user_details', (table) => {
+        table.string('domaine')
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    knex.schema.table('user_details', (table) => {
+        table.dropColumn('domaine');
+    });
+};
