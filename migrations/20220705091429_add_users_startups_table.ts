db7dbf1a5f79e03fc3d3b5005f1876de3d215c61
Charrier <lucas.charrier@beta.gouv.fr>
2022-07-05 11:43:43 +0200

    feat(migration): added new table users_startups (#1469)
    * feat(migration): added new table users_startups
    
    Added new table users_startups that link a startup to a user
    
    * Update syncBetagouvAPIScheduler.ts
    
    * Update syncBetagouvAPIScheduler.ts

---
+++ migrations/20220705091429_add_users_startups_table.ts
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('users_startups', (table) => {
+        table.text('user_id')
+        table.text('startup_id')
+        table.primary(['user_id', 'startup_id']);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    knex.schema.dropTable('users_startups')
+};
