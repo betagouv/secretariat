126407c02e0887a48b51d07c17100397afad4a8a
lucharrier <lucharrier@gmail.com>
2022-07-19 10:10:25 +0200

    Removed unused var

--- src/views/pages/components/CommuneSelect.tsx
+++ src/views/pages/components/CommuneSelect.tsx
@@ -4,7 +4,6 @@ import AsyncSelect from 'react-select/async';
 import { ClientOnly } from "./ClientOnly"
 
 export default ({ loadOptions, defaultValue, onChange, placeholder }) => {
-    const [input, setInput] = useState(defaultValue || '');
 
     return <ClientOnly>
         <AsyncSelect
@@ -12,8 +11,8 @@ export default ({ loadOptions, defaultValue, onChange, placeholder }) => {
             loadOptions={loadOptions}
             defaultOptions
             placeholder={placeholder}
-            defaultInputValue={input}
-            defaultValue={input}
+            defaultInputValue={defaultValue}
+            defaultValue={defaultValue}
             hideSelectedOptions={false}
             onChange={onChange}
             blurInputOnSelect={false}
