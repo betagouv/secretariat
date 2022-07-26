2a5a87c15193b1180b87fcf1b6c8eaaf4dc07178
Charrier <lucas.charrier@beta.gouv.fr>
2022-07-26 11:07:00 +0200

    Feat (hookController.ts) : add link to conversation in chatwoot #1538
    close #1538

--- src/controllers/hookController.ts
+++ src/controllers/hookController.ts
@@ -3,7 +3,15 @@ import config from "../config"
 
 export const postToHook = async (req, res) => {
     if (req.params.hookId === config.CHATWOOT_ID) {
-      const message = `:toolbox: Nouvelle demande de support de ${req.body.meta.sender.email} : ${req.body.messages.map(m => m.content).join('/n')}`
+      let conversationId = ''
+      try {
+        conversationId = req.body.meta.conversation.display_id
+      } catch (e) {
+        console.error('Could not get conversation Id')
+      }
+      const message = `:toolbox: Nouvelle demande de support de ${req.body.meta.sender.email} : ${req.body.messages.map(m => m.content).join('/n')}
+       /n https://chatwoot.incubateur.net/app/accounts/1/inbox/1/conversations/${conversationId}
+      `
       console.log(`Post message : `, message) // Call your action on the request here
       await axios.post(`https://mattermost.incubateur.net/hooks/${config.CHATWOOT_ID}`, {text: message});
     }
