ddc98457cbd9c36f4dbcb93e6562f0577bdebafb
Charrier <lucas.charrier@beta.gouv.fr>
2022-08-01 22:30:19 +0200

    Improv (Community Page) : implement better user research (#1569)
    * Improv (Community Page) : implement better user research
    
    * Added CommunityComponent files
    
    * Fixed typescript props
    
    * Replace referents by members
    
    * Replace referents by members
    
    * Replace referents by members
    
    * Fixed css that hide select and add missing send build page

---
+++ src/views/pages/CommunityPage/Community.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import type { Request } from 'express'
+import { Member } from 'src/models/member';
+import { InnerPageLayout } from '../components/InnerPageLayout';
+import { hydrateOnClient } from '../../hydrateOnClient'
+import MemberSelect from '../components/MemberSelect';
+  
+interface CommunityProps {
+    title: string,
+    currentUserId: string,
+    errors: string[],
+    messages: string[],
+    users: Member[],
+    activeTab: string,
+    request: Request,
+}
+  
+const css = ".panel { min-height: 100vh; }"
+
+/* Pure component */
+export const Community = InnerPageLayout((props: CommunityProps) => {
+    const [state, setState] = React.useState<any>({
+        selectedName: '',
+        ...props,
+    });
+    const changeFormData = (key, value) => {
+        setState({
+            ...state,
+            key: value
+        })
+    }
+
+    return (
+    <>
+        <div className="module">
+            <div className="panel panel-full-width">
+                <h3>
+                    Rechercher
+                </h3>
+                <form className="no-margin" action="/community">
+                    <div className="form__group">
+                        <label><strong>Nom ou prénom du membre</strong></label>
+                        <MemberSelect
+                            name="username"
+                            onChange={(e) => changeFormData('username', e.value)}
+                            members={props.users.map(u => ({
+                                value: u.id,
+                                label: u.fullname
+                            }))}
+                            defaultValue={undefined}></MemberSelect>
+                    </div>
+                    <div className="form__group">
+                        <button className="button no-margin" type="submit">Voir la fiche</button>
+                    </div>
+                </form>
+                <br />
+                <p>
+                    Le membre que vous cherchez n'existe pas ? Vous pouvez lui donner le lien du <a href="/onboarding">formulaire de création de fiche</a>.
+                </p>
+            </div>
+        </div>
+        <style media="screen">
+            {css}
+        </style>
+    </>)
+})
+
+hydrateOnClient(Community)
