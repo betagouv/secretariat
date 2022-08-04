ae95163b8cf269c4ae136b275ec0bd87711b7628
Lucas Charrier <lucharrier@gmail.com>
2022-08-04 16:07:30 +0200

    Added test file

---
+++ tests/test-service-marrainage.ts
@@ -0,0 +1,51 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import sinon from 'sinon';
+import _ from 'lodash/array';
+import * as controllerUtils from '../src/controllers/utils';
+import knex from '../src/db';
+import testUsers from './users.json';
+import { MarrainageService1v, MarrainageService2v } from '../src/services/marrainageService';
+import { Domaine, Member } from '../src/models/member';
+
+
+chai.use(chaiHttp);
+
+describe('Marrainage', () => {
+  let clock;
+  let sendEmailStub;
+  let differenceLodashSpy;
+
+  beforeEach((done) => {
+    sendEmailStub = sinon
+      .stub(controllerUtils, 'sendMail')
+      .returns(Promise.resolve(true));
+    differenceLodashSpy = sinon
+      .spy(_,'differenceWith')
+    
+    clock = sinon.useFakeTimers(new Date('2020-01-01T09:59:59+01:00'));
+    done();
+  });
+
+  afterEach((done) => {
+    knex('marrainage')
+      .truncate()
+      .then(() => sendEmailStub.restore())
+      .then(() => clock.restore())
+      .then(() => done());
+    differenceLodashSpy.restore();
+    sendEmailStub.restore();
+  });
+
+    describe('unauthenticated', () => {
+        it('Test marrainageService v1', (done) => {
+            const marrainageService = new MarrainageService1v()
+            const onboarder = marrainageService.selectRandomOnboarder('lucas.charrier', Domaine.DEVELOPPEMENT)
+        });
+
+        it('Test marrainageService v2', (done) => {
+            const marrainageService = new MarrainageService2v(testUsers as Member[])
+            const onboarder = marrainageService.selectRandomOnboarder('lucas.charrier', Domaine.DEVELOPPEMENT)
+        });
+    });
+});
