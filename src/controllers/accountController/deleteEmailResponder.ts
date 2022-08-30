70d0475dd61c7bd1ea2dd39dd638e6eea3a31789
Lucas Charrier <lucharrier@gmail.com>
2022-08-30 15:24:59 +0200

    Refacto : turn file accountController into a folder

---
+++ src/controllers/accountController/deleteEmailResponder.ts
@@ -0,0 +1,18 @@
+import betagouv from "@/betagouv";
+import { addEvent, EventCode } from '@lib/events'
+
+export async function deleteEmailResponder(req, res) {
+    try {
+      await betagouv.deleteResponder(req.auth.id)
+      addEvent(EventCode.MEMBER_RESPONDER_DELETED, {
+        created_by_username: req.auth.id,
+        action_on_username: req.auth.id
+      })
+    } catch(err) {
+      const errorMessage = `Une erreur est intervenue lors de la suppression de la réponse automatique : ${err}`
+      console.error(errorMessage);
+      req.flash('error', errorMessage);
+    }
+    return res.redirect('/account');
+  }
+  
