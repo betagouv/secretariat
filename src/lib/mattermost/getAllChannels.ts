45d548f13ee8b1a70fbe43612971c30386c61bb9
Charrier <lucas.charrier@beta.gouv.fr>
2022-09-12 15:14:49 +0200

    Feat : get all chanels using shared chanels (#1673)
    * Feat : get all chanels using shared chanels
    
    * Test : fixed tests
    
    * Fix : add teamId to get all mattermost channel
    
    * Feat : add team_id in params

--- src/lib/mattermost/getAllChannels.ts
+++ src/lib/mattermost/getAllChannels.ts
@@ -2,13 +2,25 @@ import config from "@/config";
 import axios from "axios";
 import { getMattermostConfig } from ".";
 
-export async function getAllChannels() {
+export async function getAllChannels(teamId : string, i : number = 0) {
     const mattermostChannels = await axios
-      .get(`${config.mattermostURL}/api/v4/channels`, {
+      .get(`${config.mattermostURL}/api/v4/sharedchannels/${teamId}`, {
+        params: {
+          per_page: 200,
+          page: i,
+          team_id: teamId
+        },
         ...getMattermostConfig(),
       })
       .then((response) => response.data);
-    return mattermostChannels
+    if (!mattermostChannels.length) {
+      return [];
+    }
+    const nextPageMattermostChannels = await getAllChannels(
+      teamId,
+      i + 1
+    );
+    return [...mattermostChannels, ...nextPageMattermostChannels];
   }
 
   
