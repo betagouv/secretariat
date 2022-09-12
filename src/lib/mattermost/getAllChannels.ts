33c270da50a5018c6c3d55e376382c4130a81cc9
Lucas Charrier <lucharrier@gmail.com>
2022-09-12 14:55:40 +0200

    Fix : add teamId to get all mattermost channel

--- src/lib/mattermost/getAllChannels.ts
+++ src/lib/mattermost/getAllChannels.ts
@@ -2,9 +2,9 @@ import config from "@/config";
 import axios from "axios";
 import { getMattermostConfig } from ".";
 
-export async function getAllChannels(i = 0) {
+export async function getAllChannels(teamId : string, i : number = 0) {
     const mattermostChannels = await axios
-      .get(`${config.mattermostURL}/api/v4/sharedchannels/`, {
+      .get(`${config.mattermostURL}/api/v4/sharedchannels/${teamId}`, {
         params: {
           per_page: 200,
           page: i
@@ -16,6 +16,7 @@ export async function getAllChannels(i = 0) {
       return [];
     }
     const nextPageMattermostChannels = await getAllChannels(
+      teamId,
       i + 1
     );
     return [...mattermostChannels, ...nextPageMattermostChannels];
