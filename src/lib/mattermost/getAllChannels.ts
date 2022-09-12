6c455cda7605475938c7c2c35152a7d6c66f2a88
Charrier <lucas.charrier@beta.gouv.fr>
2022-09-12 16:49:56 +0200

    Fix : use the pubic channel api endpoint of mattermost to fetch all c… (#1675)
    * Fix : use the pubic channel api endpoint of mattermost to fetch all channels
    
    * Fix : give params mattermot team id

--- src/lib/mattermost/getAllChannels.ts
+++ src/lib/mattermost/getAllChannels.ts
@@ -2,19 +2,26 @@ import config from "@/config";
 import axios from "axios";
 import { getMattermostConfig } from ".";
 
-export async function getAllChannels() {
+export async function getAllChannels(teamId: string, i : number = 0) {
   try {
     const mattermostChannels = await axios
-      .get(`${config.mattermostURL}/api/v4/channels/`, {
+      .get(`${config.mattermostURL}/api/v4/teams/${teamId}/channels`, {
+        params: {
+          per_page: 200,
+          page: i
+        },
         ...getMattermostConfig(),
       })
       .then((response) => response.data);
     if (!mattermostChannels.length) {
       return [];
     }
-    return mattermostChannels
+    const nextPageMattermostChannels = await getAllChannels(
+      teamId,
+      i + 1
+    );
+    return [...mattermostChannels, ...nextPageMattermostChannels];
     } catch(e) {
-      console.log(`Mattermost : Errors while gettings channels ${e}`)
       return []
     }
   }
