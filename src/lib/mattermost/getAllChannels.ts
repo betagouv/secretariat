3570bbef321ba6ce218b617e1810bf8565a407b2
Lucas Charrier <lucharrier@gmail.com>
2022-09-12 14:31:06 +0200

    Feat : get all chanels using shared chanels

--- src/lib/mattermost/getAllChannels.ts
+++ src/lib/mattermost/getAllChannels.ts
@@ -2,13 +2,23 @@ import config from "@/config";
 import axios from "axios";
 import { getMattermostConfig } from ".";
 
-export async function getAllChannels() {
+export async function getAllChannels(i = 0) {
     const mattermostChannels = await axios
-      .get(`${config.mattermostURL}/api/v4/channels`, {
+      .get(`${config.mattermostURL}/api/v4/sharedchannels/`, {
+        params: {
+          per_page: 200,
+          page: i
+        },
         ...getMattermostConfig(),
       })
       .then((response) => response.data);
-    return mattermostChannels
+    if (!mattermostChannels.length) {
+      return [];
+    }
+    const nextPageMattermostChannels = await getAllChannels(
+      i + 1
+    );
+    return [...mattermostChannels, ...nextPageMattermostChannels];
   }
 
   
