bf0fe137d824d06058cfe6507a6e9529c7485866
Lucas Charrier <lucharrier@gmail.com>
2022-09-27 12:06:53 +0200

    Feat : construct email header decolarated from mailjet

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -6,7 +6,20 @@ import htmlBuilder from '@modules/htmlbuilder/htmlbuilder'
 
 let sendEmail: SendEmail = fakeSendEmail
 
-const EMAIL_CONFIG =  {
+export const buildEmailHeader = {
+  mailjet: {
+    standart: { 'X-Mailjet-TrackOpen': '0', 'X-Mailjet-TrackClick': '0' },
+    campaign: (id) => {
+      return {
+        'X-Mailjet-Campaign': id,
+        'X-Mailjet-TrackOpen': '1',
+        'X-Mailjet-TrackClick': '1',
+      }
+    }
+  }
+}
+
+export const EMAIL_CONFIG =  {
     MAIL_DEBUG: process.env.MAIL_DEBUG,
     MAIL_HOST: process.env.MAIL_HOST,
     MAIL_IGNORE_TLS: process.env.MAIL_IGNORE_TLS,
@@ -16,7 +29,7 @@ const EMAIL_CONFIG =  {
     MAIL_SERVICE: process.env.MAIL_SERVICE,
     MAIL_USER: process.env.MAIL_USER,
     SIB_APIKEY_PUBLIC: process.env.SIB_APIKEY_PUBLIC,
-    SIB_APIKEY_PRIVATE: process.env.SIB_APIKEY_PRIVATE
+    SIB_APIKEY_PRIVATE: process.env.SIB_APIKEY_PRIVATE,
 }
 
 const {
@@ -48,7 +61,7 @@ if (process.env.NODE_ENV !== 'test') {
         MAIL_PORT,
         MAIL_SENDER,
         MAIL_SERVICE,
-        MAIL_SERVICE_HEADERS: { 'X-Mailjet-TrackOpen': '0', 'X-Mailjet-TrackClick': '0' },
+        MAIL_SERVICE_HEADERS: buildEmailHeader[EMAIL_CONFIG.MAIL_SERVICE]['standart'](),
         MAIL_USER,
         htmlBuilder
     })
