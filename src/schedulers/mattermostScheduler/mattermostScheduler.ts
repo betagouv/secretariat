6b488163bb23751d7465c1c536a9bce2fd187e4e
Lucas Charrier <lucharrier@gmail.com>
2022-10-03 15:47:25 +0200

    Feat : add script to send message to user with unallowed emails on mattermost

--- src/schedulers/mattermostScheduler/mattermostScheduler.ts
+++ src/schedulers/mattermostScheduler/mattermostScheduler.ts
@@ -7,6 +7,7 @@ import { MemberWithPrimaryEmail, Member } from '@/models/member';
 import knex from '@/db';
 import * as utils from '@controllers/utils';
 import * as mattermost from '@/lib/mattermost';
+import { sendInfoToChat } from '@/infra/chat';
 
 const mergedMemberAndDBUser = (user: Member, dbUser: DBUser) => {
     return {
@@ -22,6 +23,27 @@ const mergedMemberAndDBUser = (user: Member, dbUser: DBUser) => {
   
   const filterActiveUser = (user) => {
     return user.primary_email && user.primary_email_status === EmailStatusCode.EMAIL_ACTIVE
+  }
+
+  export const sendMessageToMattermostUsersWithUnallowedEmails = async(teamId: string) : Promise<void> => {
+    const allMattermostUsers : MattermostUser[] = await mattermost.getUserWithParams({
+      in_team: teamId
+    });
+    const usersWithUnallowedEmails = allMattermostUsers.filter(async user => {
+      const isPublicServiceEmail = await utils.isPublicServiceEmail(user.email)
+      return !isPublicServiceEmail
+    })
+    console.log('Users with unallowed emails', usersWithUnallowedEmails);
+    if (process.env.FEATURE_SHOULD_SEND_MESSAGE_TO_USER_WITH_UNALLOWED_EMAIL) {
+      for (const user of usersWithUnallowedEmails) {
+        await sendInfoToChat({
+          text: `Bonjour, cette espace mattermost (espace Communauté) n'est autorisé que pour les personnes ayant une adresse d'agent public.
+    Généralement une adresse @beta.gouv.fr. Tu as probablement changée ton adresse sans le savoir. 
+    Nous t'invitons à utiliser ton adresse d'agent public, sinon ton compte risque d'être désactivé.`,
+          username: user.username
+        })
+      }
+    }
   }
 
   export const getActiveGithubUsersUnregisteredOnMattermost = async () : Promise<MemberWithPrimaryEmail[]> => {
