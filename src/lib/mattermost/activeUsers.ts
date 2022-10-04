9b96a12767138672e94006954e610ade77c55f5c
Lucas Charrier <lucharrier@gmail.com>
2022-10-04 09:31:37 +0200

    Feat : add deactivate user with unallowed email

---
+++ src/lib/mattermost/activeUsers.ts
@@ -0,0 +1,18 @@
+import config from "@/config";
+import { getMattermostConfig } from "@/config/mattermost.config";
+import axios from "axios";
+
+export async function activeUsers(userId) {
+    try {
+      const payload = { active: true };
+      const response = await axios.put(
+        `${config.mattermostURL}/api/v4/users/${userId}/active`,
+        payload,
+        getMattermostConfig()
+      );
+      console.log(`Le compte mattermost ${userId} a été activé`);
+      return response.data;
+    } catch (err) {
+      console.error("Erreur d'activation de l‘utilisateurs à mattermost", err);
+    }
+  }
