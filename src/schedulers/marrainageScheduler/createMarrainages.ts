f16867c796fc84bea08bb5b273693722c80cad46
Lucas Charrier <lucharrier@gmail.com>
2022-11-08 17:47:59 +0100

    Fix : check that user is active before creating marrainage

--- src/schedulers/marrainageScheduler/createMarrainages.ts
+++ src/schedulers/marrainageScheduler/createMarrainages.ts
@@ -3,14 +3,21 @@ import _ from 'lodash/array';
 import knex from '@/db';
 import { createRequestForUser } from '@controllers/marrainageController';
 import { DBUser, EmailStatusCode } from '@models/dbUser';
+import betagouv from '@/betagouv';
+import { Member } from '@/models/member';
+import { getActiveUsers } from '@/controllers/utils';
 
 export async function createMarrainages() {
     console.log('Demarrage du cron job pour créer les marrainages');
     // before this date not every user had marrainage but we don't need to create for them now
     const dateFeatureAdded = new Date('01/23/2022');
+    let members : Member[] = await betagouv.usersInfos()
+    members = getActiveUsers(members)
     const users : DBUser[] = await knex('users').where({
       primary_email_status: EmailStatusCode.EMAIL_ACTIVE,
-    }).andWhere('created_at', '>', dateFeatureAdded)
+    })
+    .andWhere('created_at', '>', dateFeatureAdded)
+    .whereIn('username', members)
     const marrainages = await knex('marrainage').whereIn('username', users.map(user => user.username))
     const userWithoutMarrainage = _.differenceWith(users, marrainages, (user, marrainage) => user.username === marrainage.username)
     const createMarrainagePromises = await userWithoutMarrainage.map(async(user: DBUser) => {
@@ -28,3 +35,4 @@ export async function createMarrainages() {
       .then(() => console.log('Cron de création de marrainage terminé'))
       .catch(console.error);
   }
+  
