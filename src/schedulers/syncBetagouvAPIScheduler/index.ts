d31e588d730ecb30c7adbf5f831e6f4af42d6b55
Charrier <lucas.charrier@beta.gouv.fr>
2022-11-28 15:35:29 +0100

    Feat : add create mailing list as a scheduler, save mailinglist in db (#1905)
    * Feat : add create mailing list as a scheduler, save mailinglist in db
    
    * Fix : removed useless imports

--- src/schedulers/syncBetagouvAPIScheduler/index.ts
+++ src/schedulers/syncBetagouvAPIScheduler/index.ts
@@ -1,6 +1,6 @@
 export * from './publishJobsWTTJToMattermost'
 export * from './sendMessageToTeamForJobOpenedForALongTime'
-export * from './syncBetagouvStartupAPI'
+export * from '../startups/syncBetagouvStartupAPI'
 export * from './syncBetagouvUserAPI'
 export * from './buildCommunityBDD'
 export * from './publishJobsToMattermost'
