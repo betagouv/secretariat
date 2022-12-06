355addae8fd52ec9ce1ee56147b9395c8d0a6823
Charrier <lucas.charrier@beta.gouv.fr>
2022-12-06 15:35:32 +0100

    Fix :  invert startDate and endDate (#1926)

--- src/schedulers/unblockEmailsThatAreActive.ts
+++ src/schedulers/unblockEmailsThatAreActive.ts
@@ -4,7 +4,7 @@ import betagouv from "@/betagouv"
 export async function unblockEmailsThatAreActive() {
     const startDate = new Date()
     const endDate = new Date()
-    endDate.setMonth(startDate.getMonth() - 6)
+    startDate.setMonth(startDate.getMonth() - 6)
     const contacts = await getAllTransacBlockedContacts({ startDate, endDate, senders: [
         'espace-membre@beta.gouv.fr',
         'espace-membre@incubateur.net',
