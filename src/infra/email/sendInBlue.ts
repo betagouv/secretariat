f21b536ae6bc76c9119f00af221f2218ffa9bbf7
Lucas Charrier <lucharrier@gmail.com>
2022-12-19 17:23:13 +0100

    Build : removed useless import

--- src/infra/email/sendInBlue.ts
+++ src/infra/email/sendInBlue.ts
@@ -1,7 +1,7 @@
 
 import config from '@/config'
 import { objectArrayToCSV } from '@/controllers/utils'
-import { EmailProps, SendEmail, SendEmailProps, AddContactsToMailingListsProps, MAILING_LIST_TYPE, SendCampaignEmailProps, IMailingService, SendCampaignEmail, RemoveContactsFromMailingListProps, UpdateContactEmailProps, Contact, EMAIL_TYPES, EmailVariants } from '@modules/email'
+import { EmailProps, SendEmail, SendEmailProps, AddContactsToMailingListsProps, MAILING_LIST_TYPE, SendCampaignEmailProps, IMailingService, SendCampaignEmail, RemoveContactsFromMailingListProps, UpdateContactEmailProps, Contact, EmailVariants } from '@modules/email'
 import SibApiV3Sdk from 'sib-api-v3-sdk'
 
 const TEMPLATE_ID_BY_TYPE: Record<EmailProps['type'], number> = {
