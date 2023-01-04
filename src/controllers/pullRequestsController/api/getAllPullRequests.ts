c8973d3e7f666e4743ffc786f63537ecf7860dfc
Lucas Charrier <lucharrier@gmail.com>
2023-01-04 15:09:38 +0100

    Fix : add PR controllers file

---
+++ src/controllers/pullRequestsController/api/getAllPullRequests.ts
@@ -0,0 +1,18 @@
+import config from "@/config";
+import * as github from '@/lib/github';
+
+export async function getAllPullRequests(req, res) {    
+    try {
+        const { data: pullRequests }  = await github.getPullRequests(
+            config.githubOrganizationName, 'beta.gouv.fr', req.params.status || 'open')
+        res.json({
+            pullRequests: pullRequests
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            error: 'Impossible de récupérer les informations sur les pull requests ouvertes.'
+        });
+    }
+}
+  
