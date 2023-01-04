1924b79f058cc50e93f623061f0b12c7212e6494
Lucas Charrier <lucharrier@gmail.com>
2023-01-04 15:53:41 +0100

    Fix : added send data

--- src/views/pages/WhatIsGoingOnWithMemberPage/WhatIsGoingOnWithMember.tsx
+++ src/views/pages/WhatIsGoingOnWithMemberPage/WhatIsGoingOnWithMember.tsx
@@ -181,7 +181,9 @@ export const UpdateEndDateScreen = function(props) {
         }
         setIsSaving(true)
         axios.post(routes.ACCOUNT_POST_BASE_INFO_FORM.replace(':username', props.user.userInfos.id), {
-            end: date
+            end: date,
+            role: props.user.userInfos.role,
+            startups: props.user.userInfos.startups
         }).then((resp) => {
             setPullRequestURL(resp.data.pr_url)
             setDateStep('waitingForPR')
