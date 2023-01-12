29068928e8518fd5ed2188ba3c01d5dc446d723e
Charrier <lucas.charrier@beta.gouv.fr>
2023-01-12 21:24:12 +0100

    Update rateLimiter.ts

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const rateLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  max: process.env.NODE_ENV === 'test' ? 10000 : 100,
+  max: process.env.NODE_ENV === 'test' ? 10000 : 10000,
   message: 'You have exceeded the 100 requests in 24 hrs limit!', 
   standardHeaders: true,
   legacyHeaders: false,
