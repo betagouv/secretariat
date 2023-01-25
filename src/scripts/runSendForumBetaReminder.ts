148668c8bbc5f7db7f6cefefa7deb1d04ccff138
Lucas Charrier <lucharrier@gmail.com>
2023-01-25 10:13:29 +0100

    Fix : change params position

--- src/scripts/runSendForumBetaReminder.ts
+++ src/scripts/runSendForumBetaReminder.ts
@@ -1,6 +1,6 @@
 import { sendForumBetaReminder } from "@/schedulers/calendarScheduler";
 
-sendForumBetaReminder().then((30, 'tmp-lucas-test') => {
+sendForumBetaReminder(30, 'tmp-lucas-test').then(() => {
     console.log('Send forum beta reminder done')
 })
 
