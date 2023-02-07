4431f89d5251180b41583517922afa663fd91a34
Lucas Charrier <lucharrier@gmail.com>
2023-02-07 09:38:40 +0100

    Conf : changed which day of the month message should be sent

--- src/schedulers/cron.ts
+++ src/schedulers/cron.ts
@@ -110,7 +110,7 @@ const mattermostJobs: Job[] = [
   {
     cronTime: '0 0 10 * * *',
     onTick: () => {
-      sendGroupDeSoutienReminder('tmp-lucas-test', 2, 1)
+      sendGroupDeSoutienReminder('tmp-lucas-test', 2, 0)
     },
     isActive: true,
     name: 'sendGroupDeSoutienReminder'
