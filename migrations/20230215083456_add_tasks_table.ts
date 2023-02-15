3077e5a6e5e87355235af128899c3bafd827c3c9
Lucas Charrier <lucharrier@gmail.com>
2023-02-15 11:10:06 +0100

    Fix : removed default for last_completed and last_failed

--- migrations/20230215083456_add_tasks_table.ts
+++ migrations/20230215083456_add_tasks_table.ts
@@ -5,8 +5,8 @@ export async function up(knex) {
         table.text('description')
         table.datetime('created_at').notNullable().defaultTo(knex.fn.now());
         table.datetime('updated_at').notNullable().defaultTo(knex.fn.now());
-        table.datetime('last_completed').notNullable().defaultTo(knex.fn.now());
-        table.datetime('last_failed').notNullable().defaultTo(knex.fn.now());
+        table.datetime('last_completed');
+        table.datetime('last_failed');
         table.text('error_message')
     });
 }
