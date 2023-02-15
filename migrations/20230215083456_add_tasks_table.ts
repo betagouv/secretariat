c86ad2ad64e6b385c36b321d5bafa02be0b69f1c
Lucas Charrier <lucharrier@gmail.com>
2023-02-15 10:20:24 +0100

    Feat : saved tasks info in db

---
+++ migrations/20230215083456_add_tasks_table.ts
@@ -0,0 +1,17 @@
+export async function up(knex) {
+    return knex.schema
+    .createTable('tasks', (table) => {
+        table.text('name').defaultTo('primary')
+        table.text('description')
+        table.datetime('created_at').notNullable().defaultTo(knex.fn.now());
+        table.datetime('updated_at').notNullable().defaultTo(knex.fn.now());
+        table.datetime('last_completed').notNullable().defaultTo(knex.fn.now());
+        table.datetime('last_failed').notNullable().defaultTo(knex.fn.now());
+        table.text('error_message')
+    });
+}
+
+export async function down(knex){
+    return knex.schema.dropTable('tasks')
+}
+
