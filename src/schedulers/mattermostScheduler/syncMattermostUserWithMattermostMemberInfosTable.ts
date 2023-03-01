82c0914f92a696022d9a4d2198465a4d53af9776
Lucas Charrier <lucharrier@gmail.com>
2023-03-01 15:04:11 +0100

    Fix : changed building query to get mattermost user

--- src/schedulers/mattermostScheduler/syncMattermostUserWithMattermostMemberInfosTable.ts
+++ src/schedulers/mattermostScheduler/syncMattermostUserWithMattermostMemberInfosTable.ts
@@ -13,21 +13,21 @@ export async function syncMattermostUserWithMattermostMemberInfosTable () {
         in_team: config.mattermostTeamId
     })
     const mattermostUserEmails : string[] = mattermostUsers.map(user => user.email)
+    const mattermostMemberInfos : mattermost.MattermostUser[] = await db('mattermost_member_infos')
     const dbUsers : DBUser[] = await db('users')
-        .whereIn('secondary_email', mattermostUserEmails)
-        .orWhereIn('primary_email', mattermostUserEmails)
-        .join('mattermost_member_infos as mattermost', 'users.username', '=', 'mattermost.username')
-        .whereNull('mattermost.username')
-    console.log('DBUser', dbUsers.map(dbUser => dbUser.primary_email)) 
-    if (process.env.FEATURE_APPLY_INSERT_MATTERMOST_USER_TO_DB === 'true') {
-        for (const dbUser of dbUsers) {
-            const mattermostUser = mattermostUsers.find(mUser => isSameUser(mUser, dbUser))
-            const mattermostMemberInfo : MattermostMemberInfo = {
-                username: dbUser.username,
-                mattermost_user_id: mattermostUser.id,
-            }
-            await db('mattermost_member_infos').insert(mattermostMemberInfo)
-            console.log(`Ajoute ${dbUser.username} à la table mattermost`)
+        .whereNotIn('username', mattermostMemberInfos.map(m => m.username))
+        .where((qb) => {
+            qb.whereIn('secondary_email', mattermostUserEmails)
+            qb.orWhereIn('primary_email', mattermostUserEmails)
+        })
+    
+    for (const dbUser of dbUsers) {
+        const mattermostUser = mattermostUsers.find(mUser => isSameUser(mUser, dbUser))
+        const mattermostMemberInfo : MattermostMemberInfo = {
+            username: dbUser.username,
+            mattermost_user_id: mattermostUser.id,
         }
+        await db('mattermost_member_infos').insert(mattermostMemberInfo)
+        console.log(`Ajoute ${dbUser.username} à la table mattermost`)
     }
 }
