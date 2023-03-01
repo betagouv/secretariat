02f1097310fd686218627a155d31f8b3cad4c0a5
Charrier <lucas.charrier@beta.gouv.fr>
2023-03-01 17:06:36 +0100

    Fix : fix get mattermost active user in team adding params in url (#2102)

--- src/schedulers/mattermostScheduler/syncMattermostUserWithMattermostMemberInfosTable.ts
+++ src/schedulers/mattermostScheduler/syncMattermostUserWithMattermostMemberInfosTable.ts
@@ -9,10 +9,7 @@ const isSameUser = (mattermostUser: mattermost.MattermostUser, dbUser: DBUser) =
 }
 
 export async function syncMattermostUserWithMattermostMemberInfosTable () {
-    const mattermostUsers : mattermost.MattermostUser[] = await mattermost.getUserWithParams({
-        in_team: config.mattermostTeamId,
-        active: true
-    })
+    const mattermostUsers : mattermost.MattermostUser[] = await mattermost.getActiveMattermostUsers({in_team: config.mattermostTeamId})
     // const mattermostUserEmails : string[] = mattermostUsers.map(user => user.email)
     const mattermostMemberInfos : mattermost.MattermostUser[] = await db('mattermost_member_infos').select()
     console.log('Mattermost users length', mattermostMemberInfos.length)
