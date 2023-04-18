a2df03941eecd95e52af884ff6275a629e8bf33f
Charrier <lucas.charrier@beta.gouv.fr>
2023-04-18 14:31:55 +0200

    Test : update script to dry run post event on mattermost

--- src/scripts/runPostEventsOnMattermost.ts
+++ src/scripts/runPostEventsOnMattermost.ts
@@ -3,9 +3,9 @@ import { postEventsOnMattermost } from "@schedulers/calendarScheduler"
 
 postEventsOnMattermost({
     numberOfDays:6,
-    calendarURL: config.CALENDAR_GIP_URL,
-    chatWebhook: config.CHAT_WEBHOOK_URL_GIP,
-    calendarPublicUrl: config.CALENDAR_GIP_PUBLIC_URL
+    calendarURL: config.CALENDAR_URL,
+    chatWebhook: process.env.CALENDAR_TEMP_WEBHOOK,
+    calendarPublicUrl: config.CALENDAR_PUBLIC_URL
 }).then(() => {
     console.log('Post events on mattermost done')
 })
