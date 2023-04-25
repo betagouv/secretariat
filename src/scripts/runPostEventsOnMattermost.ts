89fc910da67d211171a63b3c0e8f02e710d0c682
Charrier <lucas.charrier@beta.gouv.fr>
2023-04-25 16:27:47 +0200

    Update runPostEventsOnMattermost.ts (#2148)

--- src/scripts/runPostEventsOnMattermost.ts
+++ src/scripts/runPostEventsOnMattermost.ts
@@ -3,9 +3,10 @@ import { postEventsOnMattermost } from "@schedulers/calendarScheduler"
 
 postEventsOnMattermost({
     numberOfDays:6,
+    canal:'general',
     calendarURL: config.CALENDAR_URL,
-    chatWebhook: process.env.CALENDAR_TEMP_WEBHOOK,
-    calendarPublicUrl: config.CALENDAR_PUBLIC_URL
+    calendarPublicUrl: config.CALENDAR_PUBLIC_URL,
+    chatWebhook: config.CHAT_WEBHOOK_URL_GENERAL
 }).then(() => {
     console.log('Post events on mattermost done')
 })
