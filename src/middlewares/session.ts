dd4c682902bfb3613dcd516da5a3e7d2f8b013c0
Lucas Charrier <lucharrier@gmail.com>
2023-05-16 17:40:01 +0200

    Feat : added session helpers

---
+++ src/middlewares/session.ts
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+import config from "@/config";
+
+export const getToken = (req) => {
+    return req.session && req.session.token || null
+}
+
+export const getJwtTokenForUser = (id) => {
+    jwt.sign({ id }, config.secret, { expiresIn: '7 days' });
+}
