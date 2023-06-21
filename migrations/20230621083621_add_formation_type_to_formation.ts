b5c720aacf1c006f9cec1f0eb2ae058e6e6b77b7
Lucas Charrier <lucharrier@gmail.com>
2023-06-21 12:17:05 +0200

    fix: added drop column airtable_id back

--- migrations/20230621083621_add_formation_type_to_formation.ts
+++ migrations/20230621083621_add_formation_type_to_formation.ts
@@ -12,6 +12,7 @@ exports.down = function(knex) {
     return knex.schema
     .alterTable('formations', async (table) => {
         table.dropColumn('formation_type');
+        table.dropColumn('formation_type_airtable_id');
     });   
 }
 
