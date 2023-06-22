6e4240a55e31aab2a7836009b0a71ae32206f9f9
Charrier <lucas.charrier@beta.gouv.fr>
2023-06-22 16:12:24 +0200

    fix: clean migrations history (#2212)

--- migrations/20230621134830_add_nb_members_to_startups.ts
+++ migrations/20230621134830_add_nb_members_to_startups.ts
@@ -1,18 +1,17 @@
 exports.up = function(knex) {
     return knex.schema
-    .alterTable('formations', (table) => {
-        table.integer('nb_active_members')
-        table.integer('nb_total_members')
-    })
+        .alterTable('startups', (table) => {
+            table.integer('nb_active_members')
+            table.integer('nb_total_members')
+            table.datetime('last_github_update')
+        })
 }
-
 
 exports.down = function(knex) {
     return knex.schema
-    .alterTable('formations', async (table) => {
+    .alterTable('startups', async (table) => {
         table.dropColumn('nb_active_members');
-        table.dropColumn('nb_total_members');
+        table.dropColumn('last_github_update');
+        table.dropColumn('nb_total_members')
     });   
 }
-
-        
