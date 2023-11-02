5029f1a057b82c3c49448d35164cc999ddcd6525
Charrier <lucas.charrier@beta.gouv.fr>
2023-11-02 18:30:20 +0100

    feat: add isAdmin info in Me api (#2260)

--- src/controllers/accountController/getCurrentUser.ts
+++ src/controllers/accountController/getCurrentUser.ts
@@ -1,7 +1,10 @@
+import { getAdmin } from '@/config/admin.config';
+
 export async function getCurrentUser(req, res) {
   return res.json({
     user: {
       name: req.auth && req.auth.id,
+      isAdmin: getAdmin().includes(req.auth.id),
     },
   });
 }
