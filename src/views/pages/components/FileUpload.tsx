f2f89738f9bcdaafe845cf9d324cfc3d26f94b45
Lucas Charrier <lucharrier@gmail.com>
2023-11-15 18:34:19 +0100

    feat: check image size and add alert message if too much

--- src/views/pages/components/FileUpload.tsx
+++ src/views/pages/components/FileUpload.tsx
@@ -1,64 +1,95 @@
-import React from "react"
+import React from 'react';
 
-const FileUpload = ({ selectedFile, setSelectedFile}) => {
-    const onFileChange = event => {
-        // Update the state
-        setSelectedFile(event.target.files[0]);
- 
-    };
+const UPLOAD_SIZE_LIMIT_IN_MO = 1;
 
-    // File content to be displayed after
-    // file upload is complete
-    let url
-    if (selectedFile) {
-        url = URL.createObjectURL(selectedFile);
+const FileUpload = ({ selectedFile, setSelectedFile }) => {
+  const onFileChange = (event) => {
+    // Update the state
+    const fileSize = event.target.files.item(0).size;
+    const fileMb = fileSize / 1024 ** 2;
+    if (fileMb < UPLOAD_SIZE_LIMIT_IN_MO) {
+      setSelectedFile(event.target.files[0]);
+    } else {
+      alert('Le fichier ne peux pas faire plus de ');
     }
+  };
 
-    const fileData = () => {
-    
-        if (url) {
-    
-            return (
-                <>
-                    <br/>
-                    <div style={{ height: 150, width: 240 }}>         
-                        <img src={url} style={{ height: 'auto', width: 'auto', maxWidth: '100%', maxHeight: '100%' }} />
-                    </div>
-                    <a onClick={() => setSelectedFile(undefined)}>supprimer 🗑️</a>
-                </> 
-            );
-        } else {
-            return
-        }
-    };
+  // File content to be displayed after
+  // file upload is complete
+  let url;
+  if (selectedFile) {
+    url = URL.createObjectURL(selectedFile);
+  }
 
-    return  <div className="form__group">
-        <label htmlFor="mission">
-            <strong>Image</strong><br />
-            Une image rectangulaire pour présenter le produit
-        </label>
-        {!selectedFile && <div style={{
-                justifyContent: 'center',
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                display: 'flex',
-                marginTop: '10px'
-            }}>
-            <input id="file" type="file" onChange={onFileChange} style={{
-                display: 'none',
-            }} />
-            <br></br>
-            <label style={{
-                border: '1px dashed #ccc',
-                display: 'inline-block',
-                padding: '6px 12px',
-                cursor: 'pointer'
+  const fileData = () => {
+    if (url) {
+      return (
+        <>
+          <br />
+          <div style={{ height: 150, width: 240 }}>
+            <img
+              src={url}
+              style={{
+                height: 'auto',
+                width: 'auto',
+                maxWidth: '100%',
+                maxHeight: '100%',
+              }}
+            />
+          </div>
+          <a onClick={() => setSelectedFile(undefined)}>supprimer 🗑️</a>
+        </>
+      );
+    } else {
+      return;
+    }
+  };
+
+  return (
+    <div className="form__group">
+      <label htmlFor="mission">
+        <strong>Image</strong>
+        <br />
+        Une image rectangulaire idéalement d'un ratio de 16:9 pour présenter le
+        produit
+      </label>
+      {!selectedFile && (
+        <div
+          style={{
+            justifyContent: 'center',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            display: 'flex',
+            marginTop: '10px',
+          }}
+        >
+          <input
+            id="file"
+            type="file"
+            accept=".png,.jpg,.jpeg"
+            onChange={onFileChange}
+            style={{
+              display: 'none',
             }}
-            htmlFor="file">Selectionne une image rectangulaire</label>
-        </div>}
+          />
+          <br></br>
+          <label
+            style={{
+              border: '1px dashed #ccc',
+              display: 'inline-block',
+              padding: '6px 12px',
+              cursor: 'pointer',
+            }}
+            htmlFor="file"
+          >
+            Selectionne une image rectangulaire
+          </label>
+        </div>
+      )}
 
-        {fileData()}
+      {fileData()}
     </div>
-}
+  );
+};
 
-export default FileUpload
+export default FileUpload;
