cb18e52db3a7596da08dc0c4c2b9ca5b203be49e
Charrier <lucas.charrier@beta.gouv.fr>
2023-11-15 17:44:45 +0100

    fix: on phase update do not delete end date if exists (#2269)

--- src/views/pages/components/StartupForm/PhaseItem.tsx
+++ src/views/pages/components/StartupForm/PhaseItem.tsx
@@ -4,52 +4,72 @@ import SEPhaseSelect from '../SEPhaseSelect';
 import DatepickerSelect from '../DatepickerSelect';
 
 interface PhaseItemProps extends Phase {
-    deletePhase(): void,
-    onChange(phase: Phase): void
+  deletePhase(): void;
+  onChange(phase: Phase): void;
 }
 
-const PhaseItem = ({ name, start, deletePhase, onChange } : PhaseItemProps) => {
-    const [startDate, setStartDate] : [Date, (Date) => void] = React.useState(start ? new Date(start) : undefined)
-    const [phase, setPhase] = React.useState(name)
-    React.useEffect(() => {
-        onChange({
-            name: phase,
-            start: startDate,
-        })
-    }, [phase, startDate])
-    return <>
-        <tr style={{ border: 'none'}}>
-            <td style={{ padding: 5 }}>                    {/* <label htmlFor="startup">
+const PhaseItem = ({
+  name,
+  start,
+  deletePhase,
+  onChange,
+  end,
+}: PhaseItemProps) => {
+  const [endDate]: [Date, (Date) => void] = React.useState(
+    end ? new Date(end) : undefined
+  );
+  const [startDate, setStartDate]: [Date, (Date) => void] = React.useState(
+    start ? new Date(start) : undefined
+  );
+  const [phase, setPhase] = React.useState(name);
+  React.useEffect(() => {
+    onChange({
+      name: phase,
+      start: startDate,
+      end: endDate,
+    });
+  }, [phase, startDate]);
+  return (
+    <>
+      <tr style={{ border: 'none' }}>
+        <td style={{ padding: 5 }}>
+          {' '}
+          {/* <label htmlFor="startup">
                         <strong>Phase</strong><br />
                     </label> */}
-                    <SEPhaseSelect
-                        onChange={(phase) => {
-                            setPhase(phase.value)
-                        }}
-                        defaultValue={name}
-                        isMulti={false}
-                        placeholder={"Selectionne la phase"}
-                    />
-                </td>
-                <td style={{ padding: 5 }}>
-                    {/* <label htmlFor="end">
+          <SEPhaseSelect
+            onChange={(phase) => {
+              setPhase(phase.value);
+            }}
+            defaultValue={name}
+            isMulti={false}
+            placeholder={'Selectionne la phase'}
+          />
+        </td>
+        <td style={{ padding: 5 }}>
+          {/* <label htmlFor="end">
                         <strong>Date de début</strong>
                     </label> */}
-                    {start && <DatepickerSelect
-                        name="startDate"
-                        min={'2020-01-31'}
-                        title="En format YYYY-MM-DD, par exemple : 2020-01-31"
-                        required
-                        dateFormat='dd/MM/yyyy'
-                        selected={startDate}
-                        onChange={(dateInput:Date) => setStartDate(dateInput)} />}
-                </td>
-                <td style={{ padding: 5 }}>
-                    <a style={{ textDecoration: 'none' }}
-                    onClick={() => deletePhase()}
-                    >🗑️</a>
-                </td>
-        </tr></>
-}
+          {start && (
+            <DatepickerSelect
+              name="startDate"
+              min={'2020-01-31'}
+              title="En format YYYY-MM-DD, par exemple : 2020-01-31"
+              required
+              dateFormat="dd/MM/yyyy"
+              selected={startDate}
+              onChange={(dateInput: Date) => setStartDate(dateInput)}
+            />
+          )}
+        </td>
+        <td style={{ padding: 5 }}>
+          <a style={{ textDecoration: 'none' }} onClick={() => deletePhase()}>
+            🗑️
+          </a>
+        </td>
+      </tr>
+    </>
+  );
+};
 
-export default PhaseItem
+export default PhaseItem;
