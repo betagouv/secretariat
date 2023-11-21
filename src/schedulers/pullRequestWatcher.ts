8112b9b0f555e4b860268e87f5825bac030b3acd
Charrier <lucas.charrier@beta.gouv.fr>
2023-11-21 17:48:36 +0100

    core: pull request watcher check also pr from 2 days ago (#2287)

--- src/schedulers/pullRequestWatcher.ts
+++ src/schedulers/pullRequestWatcher.ts
@@ -219,7 +219,10 @@ const pullRequestWatcherSendEmailToTeam = async () => {
   );
   const filteredPullRequests = pullRequests.filter((pr) => {
     const createdDate = new Date(pr.created_at);
-    return filterUpdateDateXdaysAgo(createdDate, 1);
+    return (
+      filterUpdateDateXdaysAgo(createdDate, 1) ||
+      filterUpdateDateXdaysAgo(createdDate, 2)
+    );
   });
   console.log(`Number of PR to check ${filteredPullRequests.length}`);
   const pullRequestCheckPromises = filteredPullRequests.map((pr) =>
