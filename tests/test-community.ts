92caf9c4e377635017135fb7fa0dcef9ca207add
Charrier <lucas.charrier@beta.gouv.fr>
2023-12-12 09:10:55 +0100

    fix(getUserApi): removed redirect if current user (#2313)

--- tests/test-community.ts
+++ tests/test-community.ts
@@ -12,7 +12,8 @@ chai.use(chaiHttp);
 describe('Community', () => {
   describe('GET /community unauthenticated', () => {
     it('should redirect to login', (done) => {
-      chai.request(app)
+      chai
+        .request(app)
         .get('/community')
         .redirects(0)
         .end((err, res) => {
@@ -25,21 +26,20 @@ describe('Community', () => {
   });
 
   describe('GET /community authenticated', () => {
+    let getToken;
 
-    let getToken
-    
     beforeEach(() => {
-      getToken = sinon.stub(session, 'getToken')
-      getToken.returns(utils.getJWT('membre.actif'))
-    })
+      getToken = sinon.stub(session, 'getToken');
+      getToken.returns(utils.getJWT('membre.actif'));
+    });
 
     afterEach(() => {
-      getToken.restore()
-    })
-
+      getToken.restore();
+    });
 
     it('should return a valid page', (done) => {
-      chai.request(app)
+      chai
+        .request(app)
         .get('/community')
         .end((err, res) => {
           res.should.have.status(200);
@@ -48,7 +48,8 @@ describe('Community', () => {
     });
 
     it('should return a valid page for an existing user', (done) => {
-      chai.request(app)
+      chai
+        .request(app)
         .get('/community/membre.parti')
         .end((err, res) => {
           res.should.have.status(200);
@@ -58,7 +59,8 @@ describe('Community', () => {
     });
 
     it('should redirect to community page if an unknown user is specified', (done) => {
-      chai.request(app)
+      chai
+        .request(app)
         .get('/community/membre.unknown')
         .redirects(0)
         .end((err, res) => {
@@ -68,19 +70,9 @@ describe('Community', () => {
         });
     });
 
-    it('should redirect to account if the user is the current user', (done) => {
-      chai.request(app)
-        .get('/community/membre.actif')
-        .redirects(0)
-        .end((err, res) => {
-          res.should.have.status(302);
-          res.header.location.should.equal('/account');
-          done();
-        });
-    });
-
     it("should show the user's information if the user exists", (done) => {
-      chai.request(app)
+      chai
+        .request(app)
         .get('/community/membre.parti')
         .end((err, res) => {
           res.text.should.include('Membre Parti');
@@ -93,25 +85,28 @@ describe('Community', () => {
     });
 
     it('should show the secondary email if it exists', async () => {
-      await knex('users').where({
-        username: 'membre.parti',
-      }).update({
-        secondary_email: 'perso@example.com',
-      })
-      
-      const res = await chai.request(app)
-        .get('/community/membre.parti')
+      await knex('users')
+        .where({
+          username: 'membre.parti',
+        })
+        .update({
+          secondary_email: 'perso@example.com',
+        });
+
+      const res = await chai.request(app).get('/community/membre.parti');
       res.text.should.include('Email secondaire : </span> perso@example.com');
-      await knex('users').where({
-        username: 'membre.parti',
-      })
-      .update({
-        secondary_email: null,
-      })
+      await knex('users')
+        .where({
+          username: 'membre.parti',
+        })
+        .update({
+          secondary_email: null,
+        });
     });
 
     it('should not show the secondary email if it does not exist', (done) => {
-      chai.request(app)
+      chai
+        .request(app)
         .get('/community/membre.parti')
         .end((err, res) => {
           res.text.should.include('Email secondaire : </span> Non renseigné');
@@ -120,30 +115,34 @@ describe('Community', () => {
     });
 
     it('should show the email creation form for email-less users', (done) => {
-      chai.request(app)
+      chai
+        .request(app)
         .get('/community/membre.parti')
         .end((err, res) => {
-          res.text.should.include('action="/users/membre.parti/email" method="POST"');
+          res.text.should.include(
+            'action="/users/membre.parti/email" method="POST"'
+          );
           done();
         });
     });
 
-    it('should prefill the secondary email for email-less users', async() => {
+    it('should prefill the secondary email for email-less users', async () => {
+      await knex('users').where({ username: 'membre.parti' }).update({
+        primary_email: '',
+        secondary_email: 'perso@example.com',
+      });
+
+      const res = await chai.request(app).get('/community/membre.parti');
+
+      res.text.should.include(
+        '<input value="perso@example.com" name="to_email"'
+      );
       await knex('users')
         .where({ username: 'membre.parti' })
         .update({
-          primary_email: '',
-          secondary_email: 'perso@example.com',
-        })
-      
-      const res = await chai.request(app)
-            .get('/community/membre.parti')
-
-      res.text.should.include('<input value="perso@example.com" name="to_email"');
-      await knex('users').where({ username: 'membre.parti'}).update({
-        primary_email: `membre.parti@${config.domain}`,
-        secondary_email: null
-      })
+          primary_email: `membre.parti@${config.domain}`,
+          secondary_email: null,
+        });
     });
 
     it('should not show the email creation form for users with existing emails', async () => {
@@ -158,10 +157,11 @@ describe('Community', () => {
       utils.mockUsers();
       utils.mockOvhRedirections();
       utils.mockOvhTime();
-      const res = await chai.request(app)
-        .get('/community/membre.parti')
+      const res = await chai.request(app).get('/community/membre.parti');
 
-      res.text.should.not.include('action="/users/membre.parti/email" method="POST">');
+      res.text.should.not.include(
+        'action="/users/membre.parti/email" method="POST">'
+      );
     });
 
     it('should not show the email creation form for users expired', (done) => {
@@ -176,24 +176,34 @@ describe('Community', () => {
       utils.mockOvhRedirections();
       utils.mockOvhTime();
 
-      chai.request(app)
+      chai
+        .request(app)
         .get('/community/membre.expire')
         .end((err, res) => {
-          res.text.should.include('Contrat de Membre Expiré arrivé à expiration');
-          res.text.should.not.include('action="/users/membre.expire/email" method="POST">');
-          res.text.should.not.include('action="/users/membre.expire/password" method="POST">');
+          res.text.should.include(
+            'Contrat de Membre Expiré arrivé à expiration'
+          );
+          res.text.should.not.include(
+            'action="/users/membre.expire/email" method="POST">'
+          );
+          res.text.should.not.include(
+            'action="/users/membre.expire/password" method="POST">'
+          );
           res.text.should.include('Le compte membre.expire est expiré.');
           done();
         });
     });
 
     it('should not show marrainage for expired users', (done) => {
-      chai.request(app)
+      chai
+        .request(app)
         .get('/community/membre.expire')
         .end((err, res) => {
           res.text.should.not.include("L'accueillir ?");
           res.text.should.not.include('Chercher un·e marrain·e');
-          res.text.should.include("La fonction marrainage n'est pas disponible pour les comptes expirés.");
+          res.text.should.include(
+            "La fonction marrainage n'est pas disponible pour les comptes expirés."
+          );
           done();
         });
     });
