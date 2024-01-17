1f6dfa9e4c9488389e289d33c828a132272e48c3
Lucas Charrier <lucharrier@gmail.com>
2024-01-17 11:52:49 +0100

    chore: add Sentry configuration for better error handling

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -1,32 +1,21 @@
 import * as Sentry from '@sentry/node';
-import { CaptureConsole as CaptureConsoleIntegration } from "@sentry/integrations";
-import config from '@config';
+import { ErrorRequestHandler } from 'express';
+import config from '@/config';
 
-export function initCaptureConsole() {
-  const logLevel = ['error'];
-  console.log(
-    `Initializing Sentry for log level "${logLevel}" and config: ${config.sentryDNS}`
-  );
-  Sentry.init({
-    dsn: config.sentryDNS as string,
-    // https://docs.sentry.io/platforms/javascript/configuration/integrations/plugin/#captureconsole
-    integrations: [new CaptureConsoleIntegration({ levels: logLevel })],
-  });
-}
-
-export function initCaptureConsoleWithHandler(app) {
-  if (config.sentryDNS) {
-    initCaptureConsole();
-
-    // RequestHandler creates a separate execution context using domains, so that every
-    // transaction/span/breadcrumb is attached to its own Hub instance
-    app.use(Sentry.Handlers.requestHandler());
-
-    // The error handler must be before any other error middleware and after all controllers
-    app.use(Sentry.Handlers.errorHandler());
-  } else {
-    console.log(
-      'Sentry was not initialized as SENTRY_DNS env variable is missing'
-    );
+export const initializeSentry = (app) => {
+  if (!config.sentryDNS) {
+    console.log('Sentry DSN not found. Sentry is not initialized.');
+    return;
   }
-}
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+};
+
+export const sentryErrorHandler: ErrorRequestHandler =
+  Sentry.Handlers.errorHandler();
